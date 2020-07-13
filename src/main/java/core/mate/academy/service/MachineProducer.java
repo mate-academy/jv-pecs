95d0d4397f56cb3847c8ac5f54071900a88ad699
Bohdan Chupika <boroda4436@users.noreply.github.com>
2020-07-13 12:57:21 +0300

    grammar fix
    Co-authored-by: KseniiaMakarova <59459757+KseniiaMakarova@users.noreply.github.com>

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface MachineProducer {
     /**
-     * In your implementation - create 2-3 models, add them into list and return
+     * In your implementations - create 2-3 models, add them to the list and return
      * @return - the list of models
      */
     List<Object> get();
