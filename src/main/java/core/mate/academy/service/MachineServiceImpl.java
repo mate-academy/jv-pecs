b34abb9b9567e81769d60a64ebc71bc597c3795a
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-08-09 14:38:56 +0300

    in progress

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,34 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService <T>{
+
+    @Override
+    public List<Machine> getAll(Class T) {
+        // return null;
+        List<Machine> machinesSubType = new ArrayList<>();
+        return machinesSubType;
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, T value) {
+        int size = machines.size();
+        for (int i = 0; i < size; i++) {
+            machines.add(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
