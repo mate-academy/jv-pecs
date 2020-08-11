72e28f184807801a79979d962ee5b2e32e4c7aa1
Kostian <pyteec.ua@i.ua>
2020-08-11 14:40:24 +0300

    added sulution

--- src/main/java/core/mate/academy/model/Track.java
+++ src/main/java/core/mate/academy/model/Track.java
@@ -5,7 +5,31 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Track extends Machine {
+    private String model;
+    private String engine;
+
     public Track() {
+    }
+
+    public Track(String model, String engine) {
+        this.model = model;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
     }
 
     @Override
---
+++ src/main/java/core/mate/academy/service/BulldozerMachineProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BulldozerMachineProducer implements MachineProducer<Bulldozer> {
+    public List<Bulldozer> get() {
+        Bulldozer firstBuldozer = new Bulldozer("CAT", "caterpillar");
+        Bulldozer secondBuldozer = new Bulldozer("Hitachi", "wheels");
+        List<Bulldozer> bulldozerList = new ArrayList<>();
+        bulldozerList.add(firstBuldozer);
+        bulldozerList.add(secondBuldozer);
+        return bulldozerList;
+    }
+}
