2c7f7c58c06c218f0c15b9f9348679781291fd06
Alkey <Sirj777sirj@gmail.com>
2020-08-11 17:58:18 +0300

    fix1

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,8 +1,10 @@
 package core.mate.academy.model;
+
 /**
  * Add some custom fields that could be only in Bulldozer
  * Do not remove no-args constructor
  */
+
 public class Bulldozer extends Machine {
 
     private String chassis;
