9c1abdf190dd5e78c0838bbfcae543921e4af7f6
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-08-11 16:18:44 +0300

    deleted Collections.fill(),  parameterize MachineProducer in each impl

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -35,7 +35,6 @@ public class MachineServiceImpl implements MachineService<Machine> {
         int size = list.size();
         for (int i = 0; i < size; i++) {
             list.add(i, value);
-
         }
     }
 
