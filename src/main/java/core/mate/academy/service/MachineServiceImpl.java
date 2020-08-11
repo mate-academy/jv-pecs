b510cf57adbf0815e2b904531a4810e3939b69d7
jlightgreen <ShvabovichJulia@gmail.com>
2020-08-11 17:14:26 +0300

    changed parametrization

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -3,7 +3,6 @@ package core.mate.academy.service;
 import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-
 import java.util.ArrayList;
 import java.util.List;
 
