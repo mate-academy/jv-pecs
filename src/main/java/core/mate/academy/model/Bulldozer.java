d77fecd409d6d292712be2fb0fed42d05bf7a7a6
glones <polivanchuk2013@gmail.com>
2020-08-11 14:10:42 +0300

    added solution

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,7 +5,31 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private int enginePower;
+    private int maxSpeed;
+
     public Bulldozer() {
+    }
+
+    public Bulldozer(int enginePower, int maxSpeed) {
+        this.enginePower = enginePower;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
     }
 
     @Override
--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,31 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private int weightBounds;
+    private int enginePower;
+
     public Excavator() {
+    }
+
+    public Excavator(int weightBounds, int enginePower) {
+        this.weightBounds = weightBounds;
+        this.enginePower = enginePower;
+    }
+
+    public int getWeightBounds() {
+        return weightBounds;
+    }
+
+    public void setWeightBounds(int weightBounds) {
+        this.weightBounds = weightBounds;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
     }
 
     @Override
