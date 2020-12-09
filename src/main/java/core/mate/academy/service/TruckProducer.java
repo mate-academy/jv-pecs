8d086611fc1e95eecc8744a2bccc4d9afec26c32
bogdan <b.kostevich@gmail.com>
2020-12-09 17:10:47 +0200

    Fixed separation in import

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -2,7 +2,6 @@ package core.mate.academy.service;
 
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Machine> {
