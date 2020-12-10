a756949db44c70ac95d7a8a7fb30c794fb4ed374
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-10 14:23:30 +0200

    Starting implementation of MachineService

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,23 @@
 package core.mate.academy.service;
 
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService {
+    @Override
+    public List<Object> getAll(Class type) {
+        return null;
+    }
+
+    @Override
+    public void fill(List<Object> machines, Object value) {
+
+    }
+
+    @Override
+    public void startWorking(List<Object> machines) {
+
+    }
 }
