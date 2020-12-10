d8c0fd9a66ddfb818ffbb9ac9498bbf5c2a8eaa2
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-10 12:00:21 +0200

    Added fields to Bulldozer and constructor with parameters

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,7 +5,32 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private int superPower;
+    private String logo;
+
     public Bulldozer() {
+    }
+
+    public Bulldozer(String name, String color, int superPower, String logo) {
+        super(name, color);
+        this.superPower = superPower;
+        this.logo = logo;
+    }
+
+    public int getSuperPower() {
+        return superPower;
+    }
+
+    public void setSuperPower(int superPower) {
+        this.superPower = superPower;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
     }
 
     @Override
