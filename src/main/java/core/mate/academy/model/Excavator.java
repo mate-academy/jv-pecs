26872d9d6421763e0e96c644bedb9a9c61e2f825
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-10 11:57:47 +0200

    Added fields to Excavator and constructor with parameters

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,32 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private double power;
+    private String superPower;
+
     public Excavator() {
+    }
+
+    public Excavator(String name, String color, double power, String superPower) {
+        super(name, color);
+        this.power = power;
+        this.superPower = superPower;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
     }
 
     @Override
