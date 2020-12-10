437a622cdfdec854b7c7b3ae563bea86fcd20a77
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-10 11:43:57 +0200

    added fields year and logo with setters and getters to Truck class

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,26 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int year;
+    private String logo;
+
     public Truck() {
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
     }
 
     @Override
