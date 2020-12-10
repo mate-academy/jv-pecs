cdea7d068a58018952fbe919ea6ee2444d1f32e7
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-10 11:51:07 +0200

    Added class MachineProducerImpl

---
+++ src/main/java/core/mate/academy/service/MachineProducerImpl.java
@@ -0,0 +1,10 @@
+package core.mate.academy.service;
+
+import java.util.List;
+
+public class MachineProducerImpl implements MachineProducer {
+    @Override
+    public List<Object> get() {
+        return null;
+    }
+}
