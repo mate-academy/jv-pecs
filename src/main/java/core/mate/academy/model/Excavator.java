cca2ae104f1183d9cf3649a175734e552daa60e9
Vitalik Kopach <apple_air@ukr.net>
2020-12-13 02:30:18 +0200

    implemented PECS pattern

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,22 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String typeOfBucket;
+
     public Excavator() {
+    }
+
+    public Excavator(String name, String color, String typeOfBucket) {
+        super(name, color);
+        this.typeOfBucket = typeOfBucket;
+    }
+
+    public String getTypeOfBucket() {
+        return typeOfBucket;
+    }
+
+    public void setTypeOfBucket(String typeOfBucket) {
+        this.typeOfBucket = typeOfBucket;
     }
 
     @Override
---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
+
+    @Override
+    public List<Bulldozer> get() {
+        List<Bulldozer> bulldozers = new ArrayList<>();
+        bulldozers.add(new Bulldozer("Crawler", "yellow", "Angle"));
+        bulldozers.add(new Bulldozer("Mini", "orange", "Straight"));
+        return bulldozers;
+    }
+}
