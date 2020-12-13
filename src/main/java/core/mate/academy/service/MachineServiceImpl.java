aef79e2044d579823e140a1edc8e0e200e432b99
Tonitrus <grom4er@gmail.com>
2020-12-13 20:47:34 +0200

    Fix: In class MachineServiceImpl add static final fields.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,25 +1,31 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Machine;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
+    private static final String TYPE_NAME_BULLDOZER = "Bulldozer";
+    private static final String TYPE_NAME_EXCAVATOR = "Excavator";
+    private static final String TYPE_NAME_Truck = "Truck";
+    private static final int COUNT_OF_FILL_LIST = 3;
 
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
-        if (type.getSimpleName().equals("Bulldozer")) {
+        if (type.getSimpleName().equals(TYPE_NAME_BULLDOZER)) {
             return new ArrayList<>(new BulldozerProducer().get());
-        } else if (type.getSimpleName().equals("Excavator")) {
+        } else if (type.getSimpleName().equals(TYPE_NAME_EXCAVATOR)) {
             return new ArrayList<>(new ExcavatorProducer().get());
-        } else {
+        } else if (type.getSimpleName().equals(TYPE_NAME_Truck)) {
             return new ArrayList<>(new TruckProducer().get());
         }
+        return new ArrayList<>();
     }
 
     @Override
     public void fill(List<? super Machine> machines, Machine value) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < COUNT_OF_FILL_LIST; i++) {
             machines.set(i, value);
         }
     }
