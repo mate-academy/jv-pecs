9bab9268788ddcacf9755ce17c80093ac73d5d78
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-15 10:17:09 +0200

    added extends to MachineProducer

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -4,7 +4,7 @@ import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.List;
 
-public class TruckProducer implements MachineProducer<TruckProducer> {
+public class TruckProducer implements MachineProducer<Truck> {
     @Override
     public List<? extends Machine> get() {
         return List.of(new Truck(),new Truck(), new Truck());
