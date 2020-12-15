db41cf94434b87301db2854e48c2cb9b019e43f0
Likh-Alex <likhachov991@protonmail.com>
2020-12-15 09:30:07 +0200

    Refactored type of list in MachineServiceImpl for specific type of machine, refactored if statements to check input type of machine, if no input in getAll() - return empty arrayList.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,6 +5,7 @@ import core.mate.academy.model.BulldozerProducer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.ExcavatorProducer;
 import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
 import core.mate.academy.model.TruckProducer;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,14 +17,18 @@ public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
-            List<? extends Machine> bulldozers = new BulldozerProducer().get();
+            List<Bulldozer> bulldozers = new BulldozerProducer().get();
             return new ArrayList<>(bulldozers);
-        } else if (type == Excavator.class) {
-            List<? extends Machine> excavators = new ExcavatorProducer().get();
+        }
+        if (type == Excavator.class) {
+            List<Excavator> excavators = new ExcavatorProducer().get();
             return new ArrayList<>(excavators);
         }
-        List<? extends Machine> trucks = new TruckProducer().get();
-        return new ArrayList<>(trucks);
+        if (type == Truck.class) {
+            List<Truck> trucks = new TruckProducer().get();
+            return new ArrayList<>(trucks);
+        }
+        return new ArrayList<>();
     }
 
     @Override
