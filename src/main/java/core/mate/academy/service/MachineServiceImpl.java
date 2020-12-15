120434f6987db8cfc27c8ba145ca41e6439aa9d2
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-15 12:07:54 +0200

    major changes in every method 'MachineService' and its implementation 'MachineServiceImpl'

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,15 +5,29 @@ import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
-    public void fill(List<? super Machine> machines, T value) {
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        if (type == Bulldozer.class) {
+            return new ArrayList<>(new BulldozerProducer().get());
+        } else if (type.equals(Excavator.class)) {
+            return new ArrayList<>(new ExcavatorProducer().get());
+        } else if (type.equals(Truck.class)) {
+            return new ArrayList<>(new TruckProducer().get());
+        } else {
+            return Collections.EMPTY_LIST;
+        }
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
@@ -23,19 +37,6 @@ public class MachineServiceImpl<T extends Machine> implements MachineService<T> 
     public void startWorking(List<? extends Machine> list) {
         for (Machine machine : list) {
             machine.doWork();
-        }
-    }
-
-    @Override
-    public List<Machine> getAll(Class<? extends Machine> type) {
-        if (type == Bulldozer.class) {
-            return new ArrayList<>(new BulldozerProducer().get());
-        } else if (type == Excavator.class) {
-            return new ArrayList<>(new ExcavatorProducer().get());
-        } else if (type == Truck.class) {
-            return new ArrayList<>(new TruckProducer().get());
-        } else {
-            return null;
         }
     }
 }
