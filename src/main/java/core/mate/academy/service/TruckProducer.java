2ead0b4747a6be84129563c4e9c156267c1e4d2d
Tetiana Chupika <mtv1301@gmail.com>
2021-03-17 19:52:57 +0200

    added solution

---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,18 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Truck;
+import java.util.List;
+
+public class TruckProducer implements MachineProducer {
+
+    private Truck truck1 = new Truck();
+    private Truck truck2 = new Truck();
+    private Truck truck3 = new Truck();
+
+    private List<Truck> trucks = List.of(truck1, truck2, truck3);
+
+    @Override
+    public List<Truck> get() {
+        return trucks;
+    }
+}
