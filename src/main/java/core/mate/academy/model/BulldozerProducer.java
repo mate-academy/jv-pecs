67f2250ff055727eda41808457614e59e872ed55
SakVladyslav <vsak@itsmartflex.com>
2021-04-07 17:14:58 +0300

    Fixed comments from approval

--- src/main/java/core/mate/academy/model/BulldozerProducer.java
+++ src/main/java/core/mate/academy/model/BulldozerProducer.java
@@ -3,15 +3,13 @@ package core.mate.academy.model;
 import core.mate.academy.service.MachineProducer;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer {
-    private final Bulldozer bulldozer1 = new Bulldozer();
-    private final Bulldozer bulldozer2 = new Bulldozer();
-    private final Bulldozer bulldozer3 = new Bulldozer();
-
-    private final List<Bulldozer> bulldozers = List.of(bulldozer1, bulldozer2, bulldozer3);
-
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
-        return bulldozers;
+        Bulldozer bulldozer1 = new Bulldozer();
+        Bulldozer bulldozer2 = new Bulldozer();
+        Bulldozer bulldozer3 = new Bulldozer();
+
+        return List.of(bulldozer1, bulldozer2, bulldozer3);
     }
 }
--- src/main/java/core/mate/academy/model/TruckProducer.java
+++ src/main/java/core/mate/academy/model/TruckProducer.java
@@ -3,15 +3,13 @@ package core.mate.academy.model;
 import core.mate.academy.service.MachineProducer;
 import java.util.List;
 
-public class TruckProducer implements MachineProducer {
-    private final Truck truck1 = new Truck();
-    private final Truck truck2 = new Truck();
-    private final Truck truck3 = new Truck();
-
-    private final List<Truck> trucks = List.of(truck1, truck2, truck3);
-
+public class TruckProducer implements MachineProducer<Truck> {
     @Override
     public List<Truck> get() {
-        return trucks;
+        Truck truck1 = new Truck();
+        Truck truck2 = new Truck();
+        Truck truck3 = new Truck();
+
+        return List.of(truck1, truck2, truck3);
     }
 }
