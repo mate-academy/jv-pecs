64e8cfea091e98db19e7e4f9724339e30f85bf57
Kostiantyn <konkos2505@gmail.com>
2021-04-18 18:18:46 +0300

    add solution

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,49 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+
+    @Override
+    public List getAll(Class type) {
+        List<? extends Machine> listOfMachines;
+        switch (type.getSimpleName()) {
+            case "Truck": {
+                listOfMachines = new TruckProducer().get();
+                break;
+            }
+            case "Bulldozer": {
+                listOfMachines = new BulldozerProducer().get();
+                break;
+            }
+            case "Excavator": {
+                listOfMachines = new ExcavatorProducer().get();
+                break;
+            }
+            default: {
+                listOfMachines = new ArrayList<>();
+                break;
+            }
+        }
+        return new ArrayList(listOfMachines);
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
