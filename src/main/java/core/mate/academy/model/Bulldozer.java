66f909bf025ed16e9c526810cb3e6eeb408c745b
Margarita Kislukhina <marharita.kislukhina@gmail.com>
2021-05-15 20:34:45 +0200

    done

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,7 +5,17 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private String model;
+
     public Bulldozer() {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 
     @Override
