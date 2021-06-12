347a0ce15c564941448fbff811c6262667205616
Борис Скворцов <dhakmar@gmail.com>
2021-06-12 10:42:47 +0300

    finished HW PECS

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,11 +1,14 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Bulldozer
- * Do not remove no-args constructor
- */
 public class Bulldozer extends Machine {
+    private int bladeServiceHours;
+
     public Bulldozer() {
+    }
+
+    public Bulldozer(String name, String color, int bladeServiceHours) {
+        super(name, color);
+        this.bladeServiceHours = bladeServiceHours;
     }
 
     @Override
--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -1,11 +1,14 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Excavator
- * Do not remove no-args constructor
- */
 public class Excavator extends Machine {
+    private int bucketVolume;
+
     public Excavator() {
+    }
+
+    public Excavator(String name, String color, int bucketVolume) {
+        super(name, color);
+        this.bucketVolume = bucketVolume;
     }
 
     @Override
