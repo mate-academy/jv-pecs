6b74da3f426c1e10c93f246edd0d676ddd14d1c4
Daniel Kuruch <kuruch1004@gmail.com>
2021-06-16 11:11:02 +0300

    refactored code

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface MachineService<T extends Machine> {
     List<T> getAll(Class<? extends T> type);
 
-    void fill(List<? super Machine> machines, T value);
+    void fill(List<? super T> machines, T value);
 
     void startWorking(List<? extends T> machines);
 }
