26fabe1a22acc6ae1c2125b269e412c63791d7cd
Пользователь <karaed.ml@mail.ru>
2021-07-08 13:17:47 +0300

    first_commit

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,46 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.Workable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        if (type == Bulldozer.class) {
+            List<Machine> bulldozer = new ArrayList<>(new BulldozerProducer().get());
+            return bulldozer;
+        }
+        if (type == Excavator.class) {
+            List<Machine> excavator = new ArrayList<>(new ExcavatorProducer().get());
+            return excavator;
+        }
+        if (type == Truck.class) {
+            List<Machine> truck = new ArrayList<>(new TruckProducer().get());
+            return truck;
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Workable> machines) {
+        for (Workable machine : machines) {
+            machine.doWork();
+        }
+    }
 }
