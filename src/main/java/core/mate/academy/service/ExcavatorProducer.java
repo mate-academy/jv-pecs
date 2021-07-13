77398a7a2e8e0f33eb56dea8f36ae56902d8a209
Dana Khromenko <danadiadius@gmail.com>
2021-07-13 11:57:44 +0300

    Added diamond signature to the MachineProducer interface and it's implementations

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -4,7 +4,7 @@ import core.mate.academy.model.Excavator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExcavatorProducer implements MachineProducer {
+public class ExcavatorProducer implements MachineProducer<Excavator> {
     private static final int BATCH_SIZE = 3;
 
     @Override
