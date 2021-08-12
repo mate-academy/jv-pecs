fb68cf8bc4921e691fdc1ef4217247828b3ae53d
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-08-12 13:51:55 +0300

    fixed solution

--- src/main/java/core/mate/academy/service/impl/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/impl/MachineServiceImpl.java
@@ -7,23 +7,28 @@ import core.mate.academy.model.Truck;
 import core.mate.academy.model.Workable;
 import core.mate.academy.service.BulldozerProducer;
 import core.mate.academy.service.ExcavatorProducer;
+import core.mate.academy.service.MachineProducer;
 import core.mate.academy.service.MachineService;
 import core.mate.academy.service.TruckProducer;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<Machine> {
+public class MachineServiceImpl implements MachineService<Machine> {
+    private static final Map<Class<? extends  Machine>, MachineProducer<? extends Machine>> map = new HashMap<>();
+
+    {
+        map.put(Bulldozer.class, new BulldozerProducer());
+        map.put(Truck.class, new TruckProducer());
+        map.put(Excavator.class, new ExcavatorProducer());
+    }
+
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
-        if (Excavator.class == type) {
-            return new ArrayList<>(new ExcavatorProducer().get());
-        }
-        if (Truck.class == type) {
-            return new ArrayList<>(new TruckProducer().get());
-        }
-        if (Bulldozer.class == type) {
-            return new ArrayList<>(new BulldozerProducer().get());
+        if (map.containsKey(type)) {
+            return new ArrayList<>(map.get(type).get());
         }
         return Collections.emptyList();
     }
