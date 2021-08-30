08d3026ccb2246c6a26b54c1cc8e993e99e042a4
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-08-30 22:19:46 +0300

    correction code

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,10 +1,9 @@
 package core.mate.academy.service;
 
-
 import core.mate.academy.model.Machine;
-
 import java.util.List;
 
 public interface MachineProducer<T extends Machine> {
+
     List<T> get();
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,11 +4,11 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
+
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
@@ -25,7 +25,7 @@ public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
     public void fill(List<? super Machine> machines, Machine value) {
-        for (int i = 0; i <machines.size(); i++) {
+        for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
     }
