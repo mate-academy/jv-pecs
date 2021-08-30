72b78a57ff7449c8ccc7fc0e2f9c761ccab2ad57
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-08-30 22:07:05 +0300

    working version

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -3,13 +3,11 @@ package core.mate.academy.service;
 import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Your implementation of MachineService.
- */
 public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
@@ -19,40 +17,23 @@ public class MachineServiceImpl implements MachineService<Machine> {
         if (type == Excavator.class) {
             return new ArrayList<>(new ExcavatorProducer().get());
         }
+        if (type == Truck.class) {
+            return new ArrayList<>(new TruckProducer().get());
+        }
         return new ArrayList<>();
     }
 
     @Override
-    public void fill(List<Object> machines, Object value) {
-
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i <machines.size(); i++) {
+            machines.set(i, value);
+        }
     }
 
     @Override
-    public void startWorking(List<Object> machines) {
-
-    }
-
-    /*public static void main(String[] args) {
-        List<Bulldozer> first = new ArrayList<>();
-        List<Object> second = new ArrayList<>();
-
-        forEachInCollection(first);
-        //forEachInCollection(second);
-
-        //fillCollection(first);
-        fillCollection(second);
-    }
-
-    public static void fillCollection(List<? super Machine> input ) {
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
-        input.add(bulldozer);
-        input.add(excavator);
-    }
-
-    public static void forEachInCollection(List<? extends Machine> input) {
-        for (Machine current : input) {
-            System.out.println(current.getName());
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();;
         }
-    }*/
+    }
 }
