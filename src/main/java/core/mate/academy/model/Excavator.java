2b68d9c35a72d183991aab778f1dbbe42dfe1036
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-08-31 10:27:31 +0300

    Test Ok

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,17 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String motorType;
+
     public Excavator() {
+    }
+
+    public String getMotorType() {
+        return motorType;
+    }
+
+    public void setMotorType(String motorType) {
+        this.motorType = motorType;
     }
 
     @Override
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,93 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T>, MachineProducer<T> {
+    Random random = new Random();
+
+    @Override
+    public List<T> getAll(Class<? extends Machine> type) {
+        List<T> list = new ArrayList<>();
+        if (type == Bulldozer.class) {
+            list = (List<T>) BulldozerProducer();
+        }
+        if (type == Excavator.class) {
+            list = (List<T>) ExcavatorProducer();
+
+        }
+        if (type == Truck.class) {
+            list = (List<T>) TruckProducer();
+        }
+        return list;
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        int size = machines.size();
+        for (int i = 0; i < size; i++){
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine: machines) {
+            machine.doWork();
+        }
+
+    }
+
+    @Override
+    public List<? extends Machine> get() {
+        return null;
+    }
+
+    @Override
+    public List<Bulldozer> BulldozerProducer() {
+        List<Bulldozer> list = new ArrayList<Bulldozer>();
+        for (int i = 0; i < 3; i++) {
+            Bulldozer bulldozer = new Bulldozer();
+            bulldozer.setChassis(CHASSIS[random.nextInt(CHASSIS_NUMBER)]);
+            bulldozer.setName(NAMES[random.nextInt(NAMES_NUMBER)]);
+            bulldozer.setColor(COLORS[random.nextInt(COLORS_NUMBER)]);
+            list.add(bulldozer);
+        }
+        return list;
+    }
+
+    @Override
+    public List<Excavator> ExcavatorProducer() {
+        List<Excavator> list = new ArrayList<Excavator>();
+        for (int i = 0; i < 3; i++) {
+            Excavator excavator = new Excavator();
+            excavator.setMotorType(MOTORTYPE[random.nextInt(MOTORTYPE_NUMBER)]);
+            excavator.setName(NAMES[random.nextInt(NAMES_NUMBER)]);
+            excavator.setColor(COLORS[random.nextInt(COLORS_NUMBER)]);
+            list.add(excavator);
+        }
+        return list;
+    }
+
+    @Override
+    public List<Truck> TruckProducer() {
+        List<Truck> list = new ArrayList<Truck>();
+        for (int i = 0; i < 3; i++) {
+            Truck truck = new Truck();
+            truck.setLoadCapacity(LOADCAPACITY[random.nextInt(LOADCAPACITY_NUMBER)]);
+            truck.setName(NAMES[random.nextInt(NAMES_NUMBER)]);
+            truck.setColor(COLORS[random.nextInt(COLORS_NUMBER)]);
+            list.add(truck);
+        }
+        return list;
+    }
 }
