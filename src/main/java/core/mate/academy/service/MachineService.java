336f8d854eba9b6a7602604118b658997044825b
Andrii Demchenko <avd998ne@gmail.com>
2021-09-02 10:54:48 +0300

    fixed empty lines

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -4,7 +4,6 @@ import core.mate.academy.model.Machine;
 import java.util.List;
 
 public interface MachineService<T extends Machine> {
-
     List<T> getAll(Class<? extends T> type);
 
     void fill(List<? super T> machines, T value);
