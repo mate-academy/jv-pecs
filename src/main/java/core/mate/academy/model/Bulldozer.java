5a40609dc00d878c03e602691560c9179a58340d
Sinelife <ya.serzhan@gmail.com>
2021-09-03 11:12:15 +0300

    add comments before empty constructors that was at the beginning

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,5 +1,9 @@
 package core.mate.academy.model;
 
+/**
+ * Add some custom fields that could be only in Bulldozer
+ * Do not remove no-args constructor
+ */
 public class Bulldozer extends Machine {
     private double weight;
     private double bladeWidth;
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,5 +1,9 @@
 package core.mate.academy.model;
 
+/**
+ * Add some custom fields that could be only in Truck
+ * Do not remove no-field constructor
+ */
 public class Truck extends Machine {
     private double cargoWeight;
     private int wheelNum;
