ec4a2cd6c3bbf55eb1daf8201ddb7891c6f2751f
Roman Legendzevych <green3942@gmail.com>
2021-09-06 18:47:45 +0300

    fixed after mentor review

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public interface MachineService<M extends Machine> {
 
-    List<M> getAll(Class<? extends Machine> machine);
+    List<M> getAll(Class<? extends M> machine);
 
-    void fill(List<? super Machine> machines, M value);
+    void fill(List<? super M> machines, M value);
 
-    void startWorking(List<? extends Machine> machines);
+    void startWorking(List<? extends M> machines);
 }
