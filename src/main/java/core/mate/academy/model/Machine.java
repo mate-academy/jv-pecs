e41b836d1fb64f9702b33b763cff7f157ae8555e
Kostia Dementiev <konstde00@gmail.com>
2021-09-08 12:20:03 +0300

    added getters and setters

--- src/main/java/core/mate/academy/model/Machine.java
+++ src/main/java/core/mate/academy/model/Machine.java
@@ -13,6 +13,18 @@ public abstract class Machine implements Workable {
         this.color = color;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     public void setColor(String color) {
         this.color = color;
     }
