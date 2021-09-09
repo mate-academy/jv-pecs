f6820947c6392d74179209861c5a41f045aa8351
Maksym Mazlov <mazlovmaxim@gmail.com>
2021-09-09 21:14:24 +0300

    Implemented java-pecs task

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -1,11 +1,57 @@
 package core.mate.academy.model;
 
+import java.time.LocalDateTime;
+
 /**
  * Add some custom fields that could be only in Excavator
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private int id;
+    private String model;
+    private String brand;
+    private LocalDateTime dateCreated;
+
     public Excavator() {
+    }
+
+    public Excavator(int id, String model, String brand, LocalDateTime dateCreated) {
+        this.id = id;
+        this.model = model;
+        this.brand = brand;
+        this.dateCreated = dateCreated;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
     }
 
     @Override
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,47 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.BulldozerProducer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.ExcavatorProducer;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.TruckProducer;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> machines = new ArrayList<>();
+        if (type == Bulldozer.class) {
+            machines = new BulldozerProducer().get();
+        }
+        if (type == Excavator.class) {
+            machines = new ExcavatorProducer().get();
+        }
+        if (type == Truck.class) {
+            machines = new TruckProducer().get();
+        }
+        return new ArrayList<>(machines);
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        int size = machines.size();
+
+        for (int i = 0; i < size; i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
