431c12497f7cc848934b3814d1f2ec0346faad23
vadim9482 <vadim9482@gmail.com>
2021-09-20 23:20:19 +0300

    All comments were deleted

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -7,9 +7,6 @@ import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Your implementation of MachineService.
- */
 public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
