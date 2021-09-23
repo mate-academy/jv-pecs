e4162dc9a73edfca1f56cac3f28ae2780c2fac6e
vadim9482 <vadim9482@gmail.com>
2021-09-23 19:21:57 +0300

    Lost StartWork Implementation was added

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -33,6 +33,8 @@ public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
     public void startWorking(List<? extends Machine> machines) {
-
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
     }
 }
