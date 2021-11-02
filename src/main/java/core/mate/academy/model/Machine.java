8786fc61ee54ddef0fd364b9fdbf1e56596f25c3
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-11-02 14:51:30 +0200

    redunant parametrization deleted

--- src/main/java/core/mate/academy/model/Machine.java
+++ src/main/java/core/mate/academy/model/Machine.java
@@ -1,6 +1,6 @@
 package core.mate.academy.model;
 
-public abstract class Machine<T> implements Workable {
+public abstract class Machine implements Workable {
     protected String name;
     protected String color;
 
