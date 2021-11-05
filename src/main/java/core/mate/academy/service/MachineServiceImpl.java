75f3730da7356fbf1bf7a6a633fc55eb1710b9d3
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-11-05 10:17:47 +0200

    change return statments

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -10,17 +10,17 @@ import java.util.List;
 public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
     @Override
     public List<T> getAll(Class<? extends T> type) {
-        MachineProducer<? extends Machine> machineProducer;
+        MachineProducer<? extends Machine> machineProducer = null;
         if (type == Bulldozer.class) {
             machineProducer = new BulldozerProducer();
         } else if (type == Truck.class) {
             machineProducer = new TruckProducer();
         } else if (type == Excavator.class) {
             machineProducer = new ExcavatorProducer();
-        } else {
-            return Collections.emptyList();
         }
-        return (List<T>) machineProducer.get();
+        return machineProducer != null
+                ? (List<T>) machineProducer.get()
+                           : Collections.emptyList();
     }
 
     @Override
