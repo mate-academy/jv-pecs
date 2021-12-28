f3c67fcc043e9e6a447a252f66362a71fc0b0d68
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2021-12-28 22:23:58 +0200

    First implementation of jv-pecs HW. Second try

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -4,7 +4,7 @@ import core.mate.academy.model.Excavator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExcavatorProducer implements MachineProducer<Excavator>  {
+public class ExcavatorProducer implements MachineProducer<Excavator> {
     @Override
     public List<Excavator> get() {
         List<Excavator> listOfListExcavator = new ArrayList<>();
