410827cc391ab7ea0fc9e8691a1bd828a8b99c2e
Maksym Pokras <mxpokras@gmail.com>
2022-01-05 21:38:50 +0200

    import section refactored

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,7 +1,8 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Truck;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
     @Override
