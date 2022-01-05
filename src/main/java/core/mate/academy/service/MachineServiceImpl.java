60808ae4256b9e9b92295a43acbaa03367bcc991
Maksym Pokras <mxpokras@gmail.com>
2022-01-05 19:59:26 +0200

    MachineService and its imlementation have been made

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -2,6 +2,7 @@ package core.mate.academy.service;
 
 import core.mate.academy.model.Machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,17 +10,38 @@ import java.util.List;
  */
 public class MachineServiceImpl implements MachineService<Machine>{
     @Override
-    public List<Machine> getAll(Class type) {
-        return (List<Machine>) new BulldozerProducer().get();
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> retVal = getProducer(type).get();
+        return new ArrayList<>(retVal);
     }
 
     @Override
     public void fill(List<? super Machine> machines, Machine value) {
-
+        int i = machines.indexOf(value);
+        if (i  > -1) {
+            machines.set(i, value);
+        } else {
+            machines.add(value);
+        }
     }
 
     @Override
     public void startWorking(List<? extends Machine> machines) {
+        for(Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 
+    private MachineProducer getProducer(Class type) {
+        if (BulldozerProducer.class.equals(type)) {
+            return new BulldozerProducer();
+        }
+        if (ExcavatorProducer.class.equals(type)) {
+            return new ExcavatorProducer();
+        }
+        if (TruckProducer.class.equals(type)) {
+            return new ExcavatorProducer();
+        }
+        throw new IllegalArgumentException("MachineProducer can not be '" + type + "\'");
     }
 }
