c41210b1c6d8c8de8311c14c31761be24cd9685d
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-01-05 21:39:23 +0200

    getAll,fill,startWorking parametrized

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
-    public List<Machine> getAll(Class type) {
+    public List<Machine> getAll(Class<? extends Machine> type) {
         List<Machine> list = new ArrayList<Machine>();
 
         if (type == Bulldozer.class) {
@@ -29,17 +29,16 @@ public class MachineServiceImpl implements MachineService<Machine> {
     }
 
     @Override
-    public void fill(List machines, Machine value) {
+    public void fill(List<? super Machine> machines, Machine value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i,value);
         }
     }
 
     @Override
-    public void startWorking(List machines) {
-        for (int i = 0; i < machines.size(); i++) {
-            Machine m = (Machine) machines.get(i);
-            m.doWork();
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine: machines) {
+            machine.doWork();
         }
     }
 }
