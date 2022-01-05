5475574ef04f5cfe4d66b1a15645d9211fe23ec3
Maksym Pokras <mxpokras@gmail.com>
2022-01-05 15:55:23 +0200

    MachineProducer and MachineService are described

---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,12 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
+    @Override
+    public List<? extends Bulldozer> get() {
+        return null;
+    }
+}
