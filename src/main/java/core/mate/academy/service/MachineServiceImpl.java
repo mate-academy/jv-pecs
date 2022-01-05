a70bc4e8cc578221ccdae81104161bae857557eb
vladsysa <vladsysa@mail.ru>
2022-01-05 12:06:00 +0100

    3 try

---
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -0,0 +1,43 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.BulldozerProducer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.ExcavatorProducer;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.TruckProducer;
+import java.util.Collections;
+import java.util.List;
+
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+
+    @Override
+    public List<T> getAll(Class<? extends T> type) {
+        MachineProducer<? extends Machine> machineProducer;
+        if (type == Bulldozer.class) {
+            machineProducer = new BulldozerProducer();
+        } else if (type == Truck.class) {
+            machineProducer = new TruckProducer();
+        } else if (type == Excavator.class) {
+            machineProducer = new ExcavatorProducer();
+        } else {
+            return Collections.emptyList();
+        }
+        return (List<T>) machineProducer.get();
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
+}
