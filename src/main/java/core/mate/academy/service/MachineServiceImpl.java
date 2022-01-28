637453663e250a3ba5389bd998a70189446167aa
sviat <svyatoslav.grinik@gmail.com>
2022-01-28 20:29:05 +0200

    all classes was implemented, all tests passed

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,48 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    private final Machine bulldozer = new Bulldozer();
+    private final Machine excavator = new Excavator();
+    private final Machine truck = new Truck();
+    private final MachineProducer<Machine> bulldozerProducer = new BulldozerProducer();
+    private final MachineProducer<Machine> excavatorProducer = new ExcavatorProducer();
+    private final MachineProducer<Machine> truckProducer = new TruckProducer();
+
+    @Override
+    public List<? extends Machine> getAll(Class type) {
+        if (type.equals(bulldozer.getClass())) {
+            return bulldozerProducer.get();
+        } else if (type.equals(excavator.getClass())) {
+            return excavatorProducer.get();
+        } else if (type.equals(truck.getClass())) {
+            return truckProducer.get();
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        int currentSize = machines.size();
+        machines.clear();
+        for (int i = 0; i < currentSize; i++) {
+            machines.add(value);
+        }
+    }
+
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,13 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Machine> {
+    @Override
+    public List<Truck> get() {
+        return List.of(new Truck(), new Truck());
+    }
+}
