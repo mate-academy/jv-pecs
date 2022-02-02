a18af595e5783bbc7e74193e13203a09d52263d2
AprioriKS <sounddummies@gmail.com>
2022-02-02 18:48:35 +0200

    add will card

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,25 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService{
+
+  @Override
+  public List<? super Machine> getAll(Class type) {
+    return null;
+  }
+
+  @Override
+  public void fill(List<? super Machine> machines, Machine value) {
+
+  }
+
+  @Override
+  public void startWorking(List<Object> machines) {
+
+  }
 }
---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,5 @@
+package core.mate.academy.service;
+
+public class TruckProducer implements MachineService {
+
+}
