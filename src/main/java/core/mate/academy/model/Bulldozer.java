9d889e0f5d856ccbe19d6fec4f43084abc125596
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-02-23 22:03:48 +0100

    - created classes bulldozer, Excavator, Truck

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,6 +5,14 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private String ladle;// ковш
+    private String caterpillars; // гусеница
+
+    public Bulldozer(String ladle, String caterpillars) {
+        this.ladle = ladle;
+        this.caterpillars = caterpillars;
+    }
+
     public Bulldozer() {
     }
 
@@ -12,4 +20,29 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started to work");
     }
+
+    public String getLadle() {
+        return ladle;
+    }
+
+    public void setLadle(String ladle) {
+        this.ladle = ladle;
+    }
+
+    public String getCaterpillars() {
+        return caterpillars;
+    }
+
+    public void setCaterpillars(String caterpillars) {
+        this.caterpillars = caterpillars;
+    }
+
+    @Override
+    public String toString() {
+        return "Bulldozer{" + "ladle='"
+                + ladle + '\''
+                + ", caterpillars='"
+                + caterpillars + '\'' + '}';
+    }
 }
+
