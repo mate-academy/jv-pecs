a2fbef47e9654e6ec2b932345def6e4963072e37
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-02-28 12:43:59 +0100

    - parametrized MachineProducer by <Bulldozer>

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -3,13 +3,13 @@ package core.mate.academy.service;
 import core.mate.academy.model.Bulldozer;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer {
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
         Bulldozer bulldozer1 = new Bulldozer("has a ladle", "has caterpillars");
         Bulldozer bulldozer2 = new Bulldozer("has not ladle", "has caterpillars");
         Bulldozer bulldozer3 = new Bulldozer("has a ladle", "has not caterpillars");
 
-        return List.of(bulldozer1, bulldozer2,bulldozer3);
+        return List.of(bulldozer1, bulldozer2, bulldozer3);
     }
 }
