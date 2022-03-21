c6307710cf2f620d3349b8d0f285572876de14c7
Aleksandr Savchenko <savchenko.arts@gmail.com>
2022-03-21 21:22:35 +0200

    After fixed

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,23 +4,25 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MachineServiceImpl implements MachineService<Machine> {
-    private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
-    private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
-    private final TruckProducer truckProducer = new TruckProducer();
+    private final Map<Class<? extends Machine>, MachineProducer<? extends Machine>> producersMap;
+
+    {
+        producersMap = new HashMap<>();
+        producersMap.put(Bulldozer.class, new BulldozerProducer());
+        producersMap.put(Excavator.class, new ExcavatorProducer());
+        producersMap.put(Truck.class, new TruckProducer());
+    }
 
     @Override
-    public List getAll(Class<? extends Machine> type) {
-        if (type == Bulldozer.class) {
-            return bulldozerProducer.get();
-        } else if (type == Excavator.class) {
-            return excavatorProducer.get();
-        } else if (type == Truck.class) {
-            return truckProducer.get();
-        }
-        return List.of();
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        MachineProducer<? extends Machine> machineProducer = producersMap.get(type);
+        return machineProducer == null ? new ArrayList<>() : new ArrayList<>(machineProducer.get());
     }
 
     @Override
@@ -31,8 +33,8 @@ public class MachineServiceImpl implements MachineService<Machine> {
     }
 
     @Override
-    public void startWorking(List<? extends Machine> list) {
-        for (Machine machine : list) {
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
             machine.doWork();
         }
     }
