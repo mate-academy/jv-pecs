3ae765940112c5e53722680e2b1620fdb3de7635
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-04-04 15:26:36 +0300

    changed method getAll using switch-case instead construction if-else

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,9 +1,7 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,19 +9,14 @@ import java.util.List;
  * Your implementation of MachineService.
  */
 public class MachineServiceImpl implements MachineService<Machine> {
+
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
-        MachineProducer bulldozer = new MachineProducerBulldozer();
-        MachineProducer excavator = new MachineProducerExcavator();
-        MachineProducer truck = new MachineProducerTruck();
-        if (type.equals(Bulldozer.class)) {
-            return bulldozer.get();
-        } else if (type.equals(Excavator.class)) {
-            return excavator.get();
-        } else if (type.equals(Truck.class)) {
-            return truck.get();
-        } else {
-            return Collections.emptyList();
+        switch (type.getSimpleName()) {
+            case "Bulldozer": return new ArrayList<>(new MachineProducerBulldozer().get());
+            case "Excavator": return new ArrayList<>(new MachineProducerExcavator().get());
+            case "Truck": return new ArrayList<>(new MachineProducerTruck().get());
+            default: return Collections.emptyList();
         }
     }
 
@@ -39,6 +32,5 @@ public class MachineServiceImpl implements MachineService<Machine> {
         for (Machine machine : machines) {
             machine.doWork();
         }
-
     }
 }
