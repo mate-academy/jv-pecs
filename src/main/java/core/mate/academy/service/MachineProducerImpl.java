e96956d26c08287dd96e3841877a028de201257f
SerhiiPankov <pankoff.serg@gmail.com>
2022-04-13 17:30:06 +0300

    add solution for jv-pecs

---
+++ src/main/java/core/mate/academy/service/MachineProducerImpl.java
@@ -0,0 +1,22 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.List;
+
+public class MachineProducerImpl implements MachineProducer {
+    private final Bulldozer bulldozer1 = new Bulldozer();
+    private final Bulldozer bulldozer2 = new Bulldozer();
+    private final Bulldozer bulldozer3 = new Bulldozer();
+    private final Truck truck1 = new Truck();
+    private final Truck truck2 = new Truck();
+    private final Excavator excavator = new Excavator();
+
+
+    @Override
+    public List<? extends Machine> get() {
+        return List.of(bulldozer1, truck1, bulldozer2, truck2, bulldozer3, excavator);
+    }
+}
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,35 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Machine;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<Machine> list = new ArrayList<>();
+        MachineProducer machineProducer = new MachineProducerImpl();
+        int size = machineProducer.get().size();
+        for (int i = 0; i < size; i++) {
+            if (machineProducer.get().get(i).getClass() == type) {
+                list.add(machineProducer.get().get(i));
+            }
+        }
+        return list.subList(0, list.size());
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
 }
