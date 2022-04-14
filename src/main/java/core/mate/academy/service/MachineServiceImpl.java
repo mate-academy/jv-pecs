2570d92addd9f9c1db3958c9d63bf95961ed7e68
SerhiiPankov <pankoff.serg@gmail.com>
2022-04-14 09:22:30 +0300

    fix bag with names and empty lines for jv-pecs

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
-
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
