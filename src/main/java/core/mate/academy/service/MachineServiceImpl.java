6115643779eb2e75ba2df4d93da8417615c345fa
Anastasia <anastasian817@gmail.com>
2022-04-19 16:53:33 +0300

    fixed mistakes after review

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
  * Your implementation of MachineService.
  */
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
