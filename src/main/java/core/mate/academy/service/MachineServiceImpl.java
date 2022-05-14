e48b91dc80bb544d6c76c8a2819fe0ca8535666d
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-05-14 19:09:14 +0300

    tests and stylechecks passed

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,12 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.*;
-
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.BulldozerProducer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.ExcavatorProducer;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.TruckProducer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,25 +49,3 @@ public class MachineServiceImpl implements MachineService {
         }
     }
 }
-
-//if (type == Bulldozer.class) {
-//BulldozerProducer bulldozerProducer = new BulldozerProducer();
-//return bulldozerProducer.get();
-//}
-//if (type == Excavator.class) {
-//ExcavatorProducer excavatorProducer = new ExcavatorProducer();
-//return excavatorProducer.get();
-//}
-//if (type == Truck.class) {
-//TruckProducer truckProducer = new TruckProducer();
-//return truckProducer.get();
-//}
-//return new ArrayList<>();
-
-//for (Machine machine: list) {
-//machine.doWork();
-//}
-
-//for (Machine machine: machines) {
-//machine = value;z
-//}
