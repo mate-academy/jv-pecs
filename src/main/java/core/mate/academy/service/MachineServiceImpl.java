08914caee9c7b1d8fe62ad23738db5e2759e620f
Igor <ZIM20093@gmail.com>
2022-05-26 22:38:40 +0300

    changed if else by switch case, added empty lines in the end of files

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,9 +1,6 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,18 +8,25 @@ import java.util.List;
  * Your implementation of MachineService.
  */
 public class MachineServiceImpl implements MachineService<Machine> {
+    private static final String BULLDOZER = "Bulldozer";
+    private static final String EXCAVATOR = "Excavator";
+    private static final String TRUCK = "Truck";
+
     @Override
     public List<Machine> getAll(Class type) {
         MachineProducer<? extends Machine> producer;
-
-        if (type == Bulldozer.class) {
-            producer = new BulldozzerProducer();
-        } else if (type == Excavator.class) {
-            producer = new ExcavatorProducer();
-        } else if (type == Truck.class) {
-            producer = new TruckProducer();
-        } else {
-            return new ArrayList<Machine>();
+        switch (type.getSimpleName()) {
+            case BULLDOZER:
+                producer = new BulldozzerProducer();
+                break;
+            case EXCAVATOR:
+                producer = new ExcavatorProducer();
+                break;
+            case TRUCK:
+                producer = new TruckProducer();
+                break;
+            default:
+                return new ArrayList<Machine>();
         }
         return (List<Machine>) producer.get();
     }
