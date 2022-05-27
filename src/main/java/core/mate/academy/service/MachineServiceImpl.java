bc4eb1ec0180064c2225ed1c19fe649cd95ffab3
Oleksii Mospan <alexey.mospan@gmail.com>
2022-05-27 13:55:53 +0300

    Make MachineService<T extends Machine>

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl<T> implements MachineService<T> {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
 
     @Override
     public List<T> getAll(Class<? extends Machine> type) {
