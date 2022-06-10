d54e70f2acf4900e99343368987f86468ee83c06
elenapokaliuk <100062370+elenapokaliuk@users.noreply.github.com>
2022-06-10 16:07:11 +0300

    - created classes BulldozerProducer, ExcavatorProducer, TruckProducer to get list of specific machines - added implementation for methods getAll, fill, startWorking - parameterized all given classes and methods

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,17 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        while (excavators.size() != 3) {
+            excavators.add(new Excavator());
+        }
+        return excavators;
+    }
+}
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,40 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        MachineProducer<? extends Machine> getMachines;
+        if (type.equals(Truck.class)) {
+            getMachines = new TruckProducer();
+        } else if (type.equals(Bulldozer.class)) {
+            getMachines = new BulldozerProducer();
+        } else if (type.equals(Excavator.class)) {
+            getMachines = new ExcavatorProducer();
+        } else {
+            return new ArrayList<>();
+        }
+        return (List<Machine>) getMachines.get();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
