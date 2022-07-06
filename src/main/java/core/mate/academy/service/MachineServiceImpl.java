a3e5ed18766ab5309244097e90ca7313449b9806
Nata <natserbo@gmail.com>
2022-07-06 16:33:42 +0300

    parameterise MachineProduser & machine service

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,23 @@
 package core.mate.academy.service;
 
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<Machine> implements MachineService<Machine> {
+    @Override
+    public List<Machine> getAll(Class type) {
+        return null;
+    }
+
+    @Override
+    public void fill(List<? super Machine> list, Object value) {
+
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> list) {
+
+    }
 }
