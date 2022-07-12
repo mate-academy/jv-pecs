ef8af54644a47625fbcca104c15d5944c7246070
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-07-12 16:50:40 +0300

    Created classes

---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,12 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Machine;
+
+import java.util.List;
+
+public class TruckProducer<T extends Machine> implements MachineProducer {
+    @Override
+    public List<T> get() {
+        return null;
+    }
+}
