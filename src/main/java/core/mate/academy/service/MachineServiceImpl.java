a9658fa4564b82c17986ba62e549479c103105ad
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-07-29 19:48:18 +0300

    Corrected the mentor's comments

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,22 +4,22 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
     @Override
-    public List getAll(Class<? extends Machine> type) {
+    public List<Machine> getAll(Class<? extends Machine> type) {
         if (Bulldozer.class.equals(type)) {
-            return new BulldozerProducer().get();
+            return new ArrayList<>(new BulldozerProducer().get());
         }
         if (Excavator.class.equals(type)) {
-            return new ExcavatorProducer().get();
+            return new ArrayList<>(new ExcavatorProducer().get());
         }
         if (Truck.class.equals(type)) {
-            return new TruckProducer().get();
+            return new ArrayList<>(new TruckProducer().get());
         } else {
-            return new LinkedList<Machine>();
+            return new ArrayList<>();
         }
     }
 
