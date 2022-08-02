34ce2ea9041ea938bd86d0f7808ade4916309cd0
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-08-02 20:15:57 +0300

    task PECS v.1.0 deleted whitespaces

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,11 +4,10 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MachineServiceImpl implements MachineService<Machine>{
+public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
