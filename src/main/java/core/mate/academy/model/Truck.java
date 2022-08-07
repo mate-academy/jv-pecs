89b0ce0c312b9d3377ca6d170d83c81b104d143f
Alina Nikiforova <0500302717a@gmail.com>
2022-08-07 14:08:20 +0200

    added Truck

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,16 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int horsepower;
+    private double engineVolume;
+
     public Truck() {
+    }
+
+    public Truck(int horsepower, double engineVolume, String color) {
+        this.horsepower = horsepower;
+        this.engineVolume = engineVolume;
+        setColor(color);
     }
 
     @Override
