5d2a96a9e034a2ff4760788b495202c8af7a2897
asha <ashajanebrown@gmail.com>
2022-08-09 16:16:26 +0300

    fixed fill() method in the MachineServiceImpl.java

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,6 +5,7 @@ import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
@@ -27,18 +28,13 @@ public class MachineServiceImpl<T extends Machine> implements MachineService<T> 
 
     @Override
     public void fill(List<? super Machine> machines, T value) {
-        int size = machines.size();
-        for (int i = 0; i < size; i++) {
-            machines.set(i, value);
-        }
+        Collections.fill(machines, value);
     }
 
     @Override
     public void startWorking(List<? extends Machine> machines) {
-        if (!machines.isEmpty()) {
-            for (Machine machine : machines) {
-                machine.doWork();
-            }
+        for (Machine machine : machines) {
+            machine.doWork();
         }
     }
 }
