ca6dedcf4e633cce5be7eb9d5df712074522c7dc
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-08-27 21:42:32 +0300

    test redone

---
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -0,0 +1,12 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Machine;
+import java.util.List;
+
+public interface MachineService<T extends Machine> {
+    List<? extends Machine> getAll(Class<? extends T> type);
+
+    void fill(List<? super T> machines, T value);
+
+    void startWorking(List<? extends T> machines);
+}
---
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -0,0 +1,39 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public List<? extends Machine> getAll(Class<? extends Machine> type) {
+        MachineProducer<? extends Machine> machineProducer;
+        if (type == Bulldozer.class) {
+            machineProducer = new BulldozerProducer();
+        } else if (type == Excavator.class) {
+            machineProducer = new ExcavatorProducer();
+        } else if (type == Truck.class) {
+            machineProducer = new TruckProducer();
+        } else {
+            return new ArrayList<>();
+        }
+        return (List<Machine>) machineProducer.get();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
+}
