077a634df68d88b692757c43f067ad657c880cc4
Ivan Litvynets <Litvyvanya@gmail.com>
2022-09-06 23:50:01 +0300

    moved fields producers

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -7,23 +7,21 @@ import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Your implementation of MachineService.
- */
 public class MachineServiceImpl implements MachineService<Machine> {
+    private MachineProducer<Bulldozer> bulldozerProducer = new BulldozerProducer();
+    private MachineProducer<Excavator> excavatorProducer = new ExcavatorProducer();
+    private MachineProducer<Truck> truckProducer = new TruckProducer();
+
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type != null) {
             if (type == Bulldozer.class) {
-                MachineProducer<Bulldozer> bulldozeeProducer = new BulldozerProducer();
-                return new ArrayList<>(bulldozeeProducer.get());
+                return new ArrayList<>(bulldozerProducer.get());
             }
             if (type == Excavator.class) {
-                MachineProducer<Excavator> excavatorProducer = new ExcavatorProducer();
                 return new ArrayList<>(excavatorProducer.get());
             }
             if (type == Truck.class) {
-                MachineProducer<Truck> truckProducer = new TruckProducer();
                 return new ArrayList<>(truckProducer.get());
             }
         }
