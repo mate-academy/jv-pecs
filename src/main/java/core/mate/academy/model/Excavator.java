98f174548c28b9b1aced410063cb26c90f26a466
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-09-07 15:36:23 +0300

    pecs implemented

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -1,15 +1,25 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Excavator
- * Do not remove no-args constructor
- */
 public class Excavator extends Machine {
+    private String brand;
+    private int horsePower;
+    private boolean isWorking;
+
     public Excavator() {
+    }
+
+    public Excavator(String brand, int horsePower) {
+        this.brand = brand;
+        this.horsePower = horsePower;
     }
 
     @Override
     public void doWork() {
-        System.out.println("Excavator started to work");
+        isWorking = true;
+        if (brand != null) {
+            System.out.println("Excavator " + brand + " started to work");
+        } else {
+            System.out.println("Excavator started to work");
+        }
     }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,46 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    private MachineProducer<Bulldozer> bulldozerProducer = new BulldozerProducer();
+    private MachineProducer<Excavator> excavatorProducer = new ExcavatorProducer();
+    private MachineProducer<Truck> truckProducer = new TruckProducer();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        if (type != null) {
+            if (type == Bulldozer.class) {
+                return new ArrayList<>(bulldozerProducer.get());
+            }
+            if (type == Excavator.class) {
+                return new ArrayList<>(excavatorProducer.get());
+            }
+            if (type == Truck.class) {
+                return new ArrayList<>(truckProducer.get());
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        if (machines != null) {
+            for (int i = 0; i < machines.size(); i++) {
+                machines.set(i, value);
+            }
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine: machines) {
+            machine.doWork();
+        }
+    }
 }
