e05a553ed3110228d1f68ec3722cee96d0f5211b
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-09-08 21:06:01 +0300

    changes after review

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -3,6 +3,6 @@ package core.mate.academy.service;
 import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer<M extends Machine> {
-    List<M> get();
+public interface MachineProducer<T extends Machine> {
+    List<T> get();
 }
