7338add4930e13df2eeb91bd43d69316e60449cc
Alkayde <morozovnikita5899@gmail.com>
2022-09-11 00:08:48 +0300

    Implemented machines producers and machine service implementation

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -1,11 +1,15 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Excavator
- * Do not remove no-args constructor
- */
 public class Excavator extends Machine {
+    private String manufacturer;
+    private int horsePower;
+
     public Excavator() {
+    }
+
+    public Excavator(String manufacturer, int horsePower) {
+        this.manufacturer = manufacturer;
+        this.horsePower = horsePower;
     }
 
     @Override
