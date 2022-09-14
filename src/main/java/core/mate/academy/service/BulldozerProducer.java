9a300cdd1197b1870d3a92a3b7294226bb189efa
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-09-14 15:17:07 +0300

    Implement MachineProducer interface

---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,22 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
+
+    @Override
+    public List<Bulldozer> get() {
+        Bulldozer bulldozerOne = new Bulldozer();
+        bulldozerOne.setName("BulldozerOne");
+        bulldozerOne.setColor("Black");
+        Bulldozer bulldozerTwo = new Bulldozer();
+        bulldozerTwo.setName("BulldozerTwo");
+        bulldozerTwo.setColor("Red");
+        Bulldozer bulldozerThree = new Bulldozer();
+        bulldozerThree.setName("BulldozerThree");
+        bulldozerThree.setColor("Green");
+        return List.of(bulldozerOne, bulldozerTwo, bulldozerThree);
+    }
+}
