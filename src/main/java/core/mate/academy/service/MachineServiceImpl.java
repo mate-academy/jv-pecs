3fb099c6f70569d9b62c6f34d1c968c0556d7f6f
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-10-01 22:30:17 +0300

    PECS solution v1

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,40 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService {
+    @Override
+    public List<? extends Machine> getAll(Class type) {
+        List<? extends Machine> machines;
+        MachineProducer<? extends Machine> machineProducer;
+        if (type.equals(Bulldozer.class)) {
+            machineProducer = new BulldozerProducer();
+        } else if (type.equals(Excavator.class)) {
+            machineProducer = new ExcavatorProducer();
+        } else if (type.equals(Truck.class)) {
+            machineProducer = new TruckProducer();
+        } else {
+            return new ArrayList<>();
+        }
+        machines = machineProducer.get();
+        return machines;
+    }
+
+    @Override
+    public void fill(List machines, Object value) {
+        machines.replaceAll(ignored -> value);
+    }
+
+    @Override
+    public void startWorking(List list) {
+
+    }
 }
