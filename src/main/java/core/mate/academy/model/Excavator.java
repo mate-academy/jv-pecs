9d6e4a627bbff88d61fd109a14d0f23b5caff168
shmax <mshveda28@gmail.com>
2022-10-03 14:00:04 +0300

    Add type-specific fields

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,11 +5,27 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private double degreeOfFreedom;
+    private double maxLiftHeight;
+
     public Excavator() {
+    }
+
+    public Excavator(double bucketVolume) {
+        this.degreeOfFreedom = bucketVolume;
     }
 
     @Override
     public void doWork() {
         System.out.println("Excavator started to work");
+    }
+
+    public double getDegreeOfFreedom() {
+        return degreeOfFreedom;
+    }
+
+    public Excavator setDegreeOfFreedom(double degreeOfFreedom) {
+        this.degreeOfFreedom = degreeOfFreedom;
+        return this;
     }
 }
