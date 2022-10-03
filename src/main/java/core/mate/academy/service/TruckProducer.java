cf67708f9dca7a2b627fa8536f15b2d47aecdbb8
shmax <mshveda28@gmail.com>
2022-10-03 15:19:01 +0300

    Fix checkstyle errors

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,7 +1,6 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Truck;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +11,6 @@ public class TruckProducer implements MachineProducer<Truck> {
         truckList = new ArrayList<>();
         truckList.add(new Truck(15d));
     }
-
 
     @Override
     public List<Truck> get() {
