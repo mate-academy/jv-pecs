c724dcb770c6d33a1948972f2575542b71430198
Andrii Shevchuk <zovten156@gmail.com>
2022-10-08 18:22:08 +0300

    implemented MachineService and methods

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,16 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+
+    private int carryingCapasity;
+    private int maxSpeed;
+
     public Truck() {
+    }
+
+    public Truck(int carryingCapasity, int maxSpeed) {
+        this.carryingCapasity = carryingCapasity;
+        this.maxSpeed = maxSpeed;
     }
 
     @Override
