8517a95e9cee75dd6d377eb3ed9e850c684b8450
NSay0 <krvipa@gmail.com>
2022-10-18 19:59:02 +0300

    Producers for Machines was created. Interfaces MachineService, MachineProducer was parametrized. Implementation of MachineService was added.

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,11 +1,17 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Truck
- * Do not remove no-field constructor
- */
 public class Truck extends Machine {
+    private int specialTruckField;
+
     public Truck() {
+    }
+
+    public void setSpecialTruckField(int specialTruckField) {
+        this.specialTruckField = specialTruckField;
+    }
+
+    public int getSpecialTruckField() {
+        return specialTruckField;
     }
 
     @Override
---
+++ src/main/java/core/mate/academy/producer/TruckProducer.java
@@ -0,0 +1,13 @@
+package core.mate.academy.producer;
+
+import core.mate.academy.model.Truck;
+import core.mate.academy.service.MachineProducer;
+
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Truck> {
+    @Override
+    public List<Truck> get() {
+        return List.of(new Truck(), new Truck(), new Truck(), new Truck());
+    }
+}
