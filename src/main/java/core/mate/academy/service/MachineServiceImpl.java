90b8fe8d8c7a148ce08c6dd21f187563f96ee75a
Pavlo.Polovyi <pasha32662@gmail.com>
2022-10-18 11:01:20 +0300

    Made changes according to mentor review.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,34 +5,31 @@ import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-/**
- * Your implementation of MachineService.
- */
 public class MachineServiceImpl implements MachineService<Machine> {
+    private final Map<Class<? extends Machine>, MachineProducer<? extends Machine>> producerMap;
+
+    {
+        producerMap = new HashMap<>();
+        producerMap.put(Bulldozer.class, new BulldozerProducer());
+        producerMap.put(Excavator.class, new ExcavatorProducer());
+        producerMap.put(Truck.class, new TruckProducer());
+    }
 
     @SuppressWarnings("unchecked")
     @Override
     public <V extends Machine> List<V> getAll(Class<? extends Machine> type) {
-        if (Bulldozer.class.equals(type)) {
-            BulldozerProducer bulldozerProducer = new BulldozerProducer();
-            return (List<V>) bulldozerProducer.get();
-        } else if (Excavator.class.equals(type)) {
-            ExcavatorProducer excavatorProducer = new ExcavatorProducer();
-            return (List<V>) excavatorProducer.get();
-        } else if (Truck.class.equals(type)) {
-            TruckProducer truckProducer = new TruckProducer();
-            return (List<V>) truckProducer.get();
-        }
-        return new ArrayList<>();
+        MachineProducer<? extends Machine> producer = producerMap.getOrDefault(type, null);
+        return producer == null ? new ArrayList<>() : (List<V>)producer.get();
     }
 
     @Override
     public void fill(List<? super Machine> machines, Machine value) {
-        for (int i = 0; i < machines.size(); i++) {
-            machines.set(i, value);
-        }
+        Collections.fill(machines, value);
     }
 
     @Override
--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
-
     @Override
     public List<Truck> get() {
         List<Truck> trucks = new ArrayList<>();
