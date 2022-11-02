ccb9e282c30257a946bd905f1c741988e5a3aa85
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-02 14:24:02 +0200

    fixes in truck producers

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,14 +1,17 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TruckProducer<Truck> implements MachineProducer<Truck> {
+public class TruckProducer implements MachineProducer<Truck> {
     private List<Truck> truckList;
 
     public TruckProducer() {
         truckList = new ArrayList<>();
+        truckList.add(new Truck());
+        truckList.add(new Truck());
+        truckList.add(new Truck());
     }
 
     @Override
