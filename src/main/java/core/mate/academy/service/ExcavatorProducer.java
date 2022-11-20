8f464b22855dd42acd2a6f79dc6d30c96536efac
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-20 22:04:52 +0200

    Produser classes were added

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        for (int i = 0; i < MODELS_CANT; i++) {
+            excavators.add(new Excavator());
+        }
+        return excavators;
+    }
+}
