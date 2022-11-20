b2bb80aeffbf25d2bf2ac08ecb7d11017801fdcc
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-20 21:57:19 +0200

    MachineService methods were created

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,15 +1,9 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Machine;
-import core.mate.academy.model.Workable;
-
 import java.util.List;
 
-public interface MachineProducer<T extends Machine> {
-    int MEMBERS_OF_LIST = 2;
-    /**
-     * In your implementations - create 2-3 models, add them to the list and return
-     * @return - the list of models
-     */
+public interface MachineProducer<T> {
+    int MODELS_CANT = 2;
+
     List<T> get();
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,40 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl{
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService {
+    @Override
+    public List getAll(Class type) {
+        List<? extends Machine> machines = new ArrayList<>();
+        if (type == Bulldozer.class) {
+            machines = new BulldozerProducer().get();
+        }
+        if (type == Excavator.class) {
+            machines = new ExcavatorProducer().get();
+        }
+        if (type == Truck.class) {
+            machines = new TruckProducer().get();
+        }
+        return machines;
+    }
+
+    @Override
+    public void fill(List machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List list) {
+        List<? extends Machine> machines = list;
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
