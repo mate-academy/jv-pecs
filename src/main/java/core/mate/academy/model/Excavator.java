e9ce2c7922ee616f9a8c9ddd4ac7a73af5716207
Sergei Matiko <adgodst@gmail.com>
2022-12-08 12:49:24 +0200

    hm-solution

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,22 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private double bucketCapacity;
+
     public Excavator() {
+    }
+
+    public Excavator(String name, String color, double bucketCapacity) {
+        super(name, color);
+        this.bucketCapacity = bucketCapacity;
+    }
+
+    public double getBucketCapacity() {
+        return bucketCapacity;
+    }
+
+    public void setBucketCapacity(double bucketCapacity) {
+        this.bucketCapacity = bucketCapacity;
     }
 
     @Override
