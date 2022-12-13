1c684ceac584f7813e3e890bad255318d4a4bee5
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-12-13 20:28:44 +0200

    solution for PECS hw was added

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,44 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+
+    private BulldozerProducer bulldozerProducer = new BulldozerProducer();
+    private ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+    private TruckProducer truckProducer = new TruckProducer();
+
+    @Override
+    public List getAll(Class type) {
+        if (type == Bulldozer.class) {
+            return new ArrayList<>(bulldozerProducer.get());
+        }
+        if (type == Excavator.class) {
+            return new ArrayList<>(excavatorProducer.get());
+        }
+        if (type == Truck.class) {
+            return new ArrayList<>(truckProducer.get());
+        }
+        return new ArrayList();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        int size = machines.size();
+        for (int i = 0; i < size; i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
