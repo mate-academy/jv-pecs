0ec128fcb6a8808ff4a3df367aeb196639a9a5cc
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-12-24 17:53:40 +0200

    if block replaced with a map

--- src/main/java/core/mate/academy/service/ProducerStrategy.java
+++ src/main/java/core/mate/academy/service/ProducerStrategy.java
@@ -4,22 +4,26 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProducerStrategy {
     private final MachineProducer<Bulldozer> bulldozerProducer = new BulldozerProducer();
     private final MachineProducer<Excavator> excavatorProducer = new ExcavatorProducer();
     private final MachineProducer<Truck> truckProducer = new TruckProducer();
+    private Map<Class<? extends Machine>, MachineProducer<? extends Machine>> map = new HashMap<>();
+
+    {
+        map.put(Bulldozer.class, bulldozerProducer);
+        map.put(Excavator.class, excavatorProducer);
+        map.put(Truck.class, truckProducer);
+    }
 
     public MachineProducer<? extends Machine> getMachineProducer(Class<? extends Machine> type) {
-        if (type == Bulldozer.class) {
-            return bulldozerProducer;
+        MachineProducer<? extends Machine> machineProducer = map.get(type);
+        if (machineProducer == null) {
+            throw new RuntimeException("Got invalid class for producer. Please, input valid class");
         }
-        if (type == Excavator.class) {
-            return excavatorProducer;
-        }
-        if (type == Truck.class) {
-            return truckProducer;
-        }
-        throw new RuntimeException("Got invalid class for producer. Please, input valid class");
+        return machineProducer;
     }
 }
