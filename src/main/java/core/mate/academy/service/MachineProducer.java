ceb7f8fa2b2b0f7111c0997bae97de94d491002a
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-25 16:39:52 +0200

    copy

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -6,6 +6,7 @@ public interface MachineProducer {
     /**
      * In your implementations - create 2-3 models, add them to the list and return
      * @return - the list of models
+     *
      */
     List<Object> get();
 }
