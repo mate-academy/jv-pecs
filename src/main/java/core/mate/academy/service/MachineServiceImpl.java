de2042c819ee4fc1d15b9714211e1e17e9a9430c
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-27 19:47:12 +0200

    third commit

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,22 +1,24 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import java.util.ArrayList;
+import core.mate.academy.model.Truck;
+import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
-        switch (type.getSimpleName()) {
-            case "Truck":
-                return new TruckProducer().get();
-            case "Bulldozer":
-                return new BulldozerProducer().get();
-            case "Excavator":
-                return new ExcavatorProducer().get();
-            default:
-                return new ArrayList<>();
+        MachineProducer producer = null;
+        if (type.equals(Truck.class)) {
+            producer = new TruckProducer();
+        } else if (type.equals(Bulldozer.class)) {
+            producer = new BulldozerProducer();
+        } else if (type.equals(Excavator.class)) {
+            producer = new ExcavatorProducer();
         }
+        return producer != null ? producer.get() : Collections.emptyList();
     }
 
     @Override
