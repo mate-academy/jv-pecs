d4cb9215f9b72afd21ff30f17428d6041fb881a6
Oleksandr Dikhtyarenko <oleksandrdihtarenko11@gmail.com>
2022-12-27 20:45:03 +0200

    Producer classes were parametrised due to their functions deleted empty line in ExcavatorProducer method get parametrised by T instead of Machine or Object

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -3,16 +3,10 @@ package core.mate.academy.service;
 import core.mate.academy.model.Excavator;
 import java.util.List;
 
-public class ExcavatorProducer implements MachineProducer {
-
-    private Excavator excavator1 = new Excavator(1, 2);
-    private Excavator excavator2 = new Excavator(2, 3);
-    private Excavator excavator3 = new Excavator(3, 4);
-
-    private List<Excavator> excavators = List.of(excavator1, excavator2, excavator3);
+public class ExcavatorProducer implements MachineProducer<Excavator> {
 
     @Override
     public List<Excavator> get() {
-        return excavators;
+        return List.of(new Excavator(), new Excavator(), new Excavator());
     }
 }
--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -3,15 +3,10 @@ package core.mate.academy.service;
 import core.mate.academy.model.Truck;
 import java.util.List;
 
-public class TruckProducer implements MachineProducer {
-    private Truck truck1 = new Truck(1,"Flatbed truck");
-    private Truck truck2 = new Truck(2,"Tipper truck");
-    private Truck truck3 = new Truck(3,"Crane truck");
-
-    private List<Truck> trucks = List.of(truck1, truck2, truck3);
+public class TruckProducer implements MachineProducer<Truck> {
 
     @Override
     public List<Truck> get() {
-        return trucks;
+        return List.of(new Truck(), new Truck(), new Truck());
     }
 }
