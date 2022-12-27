b98b3634609674d925e671454833450af4596bad
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-27 10:06:08 +0200

    check and repair

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,9 +1,8 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Machine;
 import java.util.List;
 
 public interface MachineProducer {
 
-    List<? extends Machine> get();
+    List<?> get();
 }
