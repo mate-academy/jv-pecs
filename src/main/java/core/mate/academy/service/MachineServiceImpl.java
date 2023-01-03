500c5684538c561111e97dae636d67207ba02098
iskamele <iskamele@gmail.com>
2023-01-03 23:24:37 +0200

    Changed Class MachineServiceImpl. Removed comment. The class implements the MachineService interface, typed <Machine>. The class-level ProducerStrategy object has been added and initialized.
    Overridden method List<Machine> getAll(Class<? extends Machine> type).
    The body of the method is wrapped in a try-catch block. Inside, we declare a producer object, which we initialize with the appropriate type using a getter, accessing the contents of the HashMap and calling the get method, passing inside the type.
    After that, we declare a List and populate it with the appropriate machine objects using producer.get().
    We return a new ArrayList, passing it to the generated List.
    If an invalid type was passed to us, we will not be able to initialize the List and instead of returning a RuntimeException error will be thrown, and an empty ArrayList will be returned as a response.
    
    Overridden method void fill(List<? super Machine> machines, Machine value).
    Inside, a loop is declared that goes through the entire List and replaces each element with a value.
    
    Overridden method void startWorking(List<? extends Machine> machines).
    Inside, a loop is declared that goes through the entire List of machines and calls the doWork () method for each of them.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,35 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.impl.ProducerStrategy;
+import core.mate.academy.model.Machine;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    private final ProducerStrategy producerStrategy = new ProducerStrategy();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        try {
+            MachineProducer<? extends Machine> producer = producerStrategy.getProducers().get(type);
+            List<? extends Machine> machines = producer.get();
+            return new ArrayList<>(machines);
+        } catch (RuntimeException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
