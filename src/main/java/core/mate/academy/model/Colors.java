3615a5319ef36ed561b298c058f3ca6ae41100b8
iskamele <iskamele@gmail.com>
2023-01-04 16:50:01 +0200

    Removed interface implementation and removed methods from ENUMs.

---
+++ src/main/java/core/mate/academy/model/Colors.java
@@ -0,0 +1,11 @@
+package core.mate.academy.model;
+
+public enum Colors {
+    WHITE,
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE,
+    BLACK
+}
---
+++ src/main/java/core/mate/academy/model/Models.java
@@ -0,0 +1,11 @@
+package core.mate.academy.model;
+
+public enum Models {
+    MAN,
+    DAF,
+    MERCEDES_BENZ,
+    SCANIA,
+    IVECO,
+    VOLVO,
+    RENAULT
+}
