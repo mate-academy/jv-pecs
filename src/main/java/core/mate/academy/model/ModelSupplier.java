29e4a690559ab937dcc70bb20934db671b7b59cc
iskamele <iskamele@gmail.com>
2023-01-04 20:57:59 +0200

    Random is now a class field. toString() replaced with name()

--- src/main/java/core/mate/academy/model/ModelSupplier.java
+++ src/main/java/core/mate/academy/model/ModelSupplier.java
@@ -3,7 +3,7 @@ package core.mate.academy.model;
 import java.util.Random;
 
 public class ModelSupplier {
-    private Random random = new Random();
+    private final Random random = new Random();
 
     public String getRandomModel() {
         int index = random.nextInt(Model.values().length);
