93174c2e517644c9dd3ce61d15f180d88abaee28
Nikita Kolesnikov <kit1datasvit@gmail.com>
2023-01-04 13:28:30 +0200

    hw-2-sulution

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,17 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private int bucketVolume;
+    private String typeСhassis;
+    private int weightMachine;
+
     public Excavator() {
+    }
+
+    public Excavator(int bucketVolume, String typeСhassis, int weightMachine) {
+        this.bucketVolume = bucketVolume;
+        this.typeСhassis = typeСhassis;
+        this.weightMachine = weightMachine;
     }
 
     @Override
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,19 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int maxSpeed;
+    private int loadCapacity;
+    private String typeTrailer;
+    private int weightMachine;
+
     public Truck() {
+    }
+
+    public Truck(int maxSpeed, int loadCapacity, String typeTrailer, int weightMachine) {
+        this.maxSpeed = maxSpeed;
+        this.loadCapacity = loadCapacity;
+        this.typeTrailer = typeTrailer;
+        this.weightMachine = weightMachine;
     }
 
     @Override
