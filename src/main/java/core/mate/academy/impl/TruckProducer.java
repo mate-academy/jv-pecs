b6cb3e17f9bd9d3852f0dbd878e6e63e005e378d
iskamele <iskamele@gmail.com>
2023-01-04 12:02:28 +0200

    Rollback from repository Old commit Added Class TruckProducer. The class implements the MachineProducer interface with `<Truck>` typing. Inside, two class-level objects ModelSupplier and ColorSupplier are declared and initialized, which are used in generating Truck objects. Overridden the get() method. Inside, we create a new List to hold the Truck objects. We add 3 Truck objects to the List, passing random model and color values to the constructor using the appropriate ModelSupplier and ColorSupplier generators. Then we return List.

--- src/main/java/core/mate/academy/impl/TruckProducer.java
+++ src/main/java/core/mate/academy/impl/TruckProducer.java
@@ -1,21 +1,20 @@
 package core.mate.academy.impl;
 
-import core.mate.academy.enums.Colors;
-import core.mate.academy.enums.Models;
 import core.mate.academy.model.Truck;
 import core.mate.academy.service.MachineProducer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
-    private static final int DEFAULT_ARRAYLIST_CAPACITY = 10;
+    private final ModelSupplier modelSupplier = new ModelSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     @Override
     public List<Truck> get() {
         List<Truck> truckList = new ArrayList<>();
-        for (int i = 0; i < DEFAULT_ARRAYLIST_CAPACITY; i++) {
-            truckList.add(new Truck(Models.random(), Colors.random()));
-        }
+        truckList.add(new Truck(modelSupplier.getRandomModel(), colorSupplier.getRandomColor()));
+        truckList.add(new Truck(modelSupplier.getRandomModel(), colorSupplier.getRandomColor()));
+        truckList.add(new Truck(modelSupplier.getRandomModel(), colorSupplier.getRandomColor()));
         return truckList;
     }
 }
