7e59ca319ac147a81c224ce1565039a737e1b3f0
iskamele <iskamele@gmail.com>
2023-01-04 12:02:03 +0200

    Rollback from repository Old commit Added Class BulldozerProducer. The class implements the MachineProducer interface with `<Bulldozer>` typing. Inside, two class-level objects ModelSupplier and ColorSupplier are declared and initialized, which are used in the generation of Bulldozer objects. Overridden the get() method. Inside, we create a new List to hold the Bulldozer objects. We add 3 Bulldozer objects to the List by passing random model and color values to the constructor using the appropriate ModelSupplier and ColorSupplier generators. Then we return List.

--- src/main/java/core/mate/academy/impl/BulldozerProducer.java
+++ src/main/java/core/mate/academy/impl/BulldozerProducer.java
@@ -1,22 +1,26 @@
 package core.mate.academy.impl;
 
-import core.mate.academy.enums.Colors;
-import core.mate.academy.enums.Models;
 import core.mate.academy.model.Bulldozer;
 import core.mate.academy.service.MachineProducer;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class BulldozerProducer implements MachineProducer<Bulldozer> {
-    private static final int DEFAULT_ARRAYLIST_CAPACITY = 10;
+    private final ModelSupplier modelSupplier = new ModelSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     @Override
     public List<Bulldozer> get() {
         List<Bulldozer> bulldozerList = new ArrayList<>();
-        for (int i = 0; i < DEFAULT_ARRAYLIST_CAPACITY; i++) {
-            bulldozerList.add(new Bulldozer(Models.random(), Colors.random()));
-        }
+        bulldozerList.add(new Bulldozer(
+                modelSupplier.getRandomModel(),
+                colorSupplier.getRandomColor()));
+        bulldozerList.add(new Bulldozer(
+                modelSupplier.getRandomModel(),
+                colorSupplier.getRandomColor()));
+        bulldozerList.add(new Bulldozer(
+                modelSupplier.getRandomModel(),
+                colorSupplier.getRandomColor()));
         return bulldozerList;
     }
 }
