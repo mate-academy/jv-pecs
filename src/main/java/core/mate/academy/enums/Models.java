40d679d7f41785534549a9537aa3da28792ade64
iskamele <iskamele@gmail.com>
2023-01-04 11:41:44 +0200

    Changed ENUM Models. The list of car models has been formatted and expanded. Now ENUM implements the Picker interface. Added a static method random() that returns a random object from this ENUM using a Picker.

--- src/main/java/core/mate/academy/enums/Models.java
+++ src/main/java/core/mate/academy/enums/Models.java
@@ -1,7 +1,15 @@
 package core.mate.academy.enums;
 
-public enum Models {
-    BMW,  
-    SKODA,  
-    MERCEDES  
+public enum Models implements Picker {
+    MAN,
+    DAF,
+    MERCEDES_BENZ,
+    SCANIA,
+    IVECO,
+    VOLVO,
+    RENAULT;
+
+    public static String random() {
+        return Picker.random(values());
+    }
 }
