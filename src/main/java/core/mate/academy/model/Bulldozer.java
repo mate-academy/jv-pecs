5e27f4c77d9358b6f735430ea8c7df84e258ffda
Алексей <m.leshka955@gmail.com>
2023-01-13 00:09:17 +0200

    implementation PECS

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,11 +1,22 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Bulldozer
- * Do not remove no-args constructor
- */
 public class Bulldozer extends Machine {
+    private boolean bucketRaised;
+    private int speed;
+    private String color;
+
     public Bulldozer() {
+    }
+
+    public Bulldozer(boolean bucketRaised, int speed, String color) {
+        this.bucketRaised = bucketRaised;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
     }
 
     @Override
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,42 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    private final MachineProducer<Bulldozer> bulldozer = new BulldozerProducer();
+    private final MachineProducer<Excavator> excavator = new ExcavatorProducer();
+    private final MachineProducer<Truck> truck = new TruckProducer();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        if (type.equals(Bulldozer.class)) {
+            return new ArrayList<>(bulldozer.get());
+        } else if (type.equals(Excavator.class)) {
+            return new ArrayList<>(excavator.get());
+        } else if (type.equals(Truck.class)) {
+            return new ArrayList<>(truck.get());
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
