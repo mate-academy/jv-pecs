f9a924401cc9b34d6b77ccb72de9fa0a7763d259
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2023-01-13 21:20:51 +0200

    fixed all review mistakes

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -1,13 +1,12 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Machine;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer<Machine> {
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
 
     @Override
-    public List<Machine> get() {
+    public List<Bulldozer> get() {
         return List.of(new Bulldozer());
     }
 }
