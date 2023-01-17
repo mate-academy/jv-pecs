a6cec3d788c2156ac390414fffcf0526b668dfb0
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-01-17 15:54:06 +0200

    added task solution

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,11 +1,31 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Bulldozer
- * Do not remove no-args constructor
- */
 public class Bulldozer extends Machine {
+    private String typeOfBlade;
+    private double bladeLiftHeight;
+
     public Bulldozer() {
+    }
+
+    public Bulldozer(String typeOfBlade, double bladeLiftHeight) {
+        this.typeOfBlade = typeOfBlade;
+        this.bladeLiftHeight = bladeLiftHeight;
+    }
+
+    public String getTypeOfBlade() {
+        return typeOfBlade;
+    }
+
+    public void setTypeOfBlade(String typeOfBlade) {
+        this.typeOfBlade = typeOfBlade;
+    }
+
+    public double getBladeLiftHeight() {
+        return bladeLiftHeight;
+    }
+
+    public void setBladeLiftHeight(double bladeLiftHeight) {
+        this.bladeLiftHeight = bladeLiftHeight;
     }
 
     @Override
---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,15 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavatorList = new ArrayList<>();
+        excavatorList.add(new Excavator("Mini", 7));
+        excavatorList.add(new Excavator("Large", 3));
+        return excavatorList;
+    }
+}
