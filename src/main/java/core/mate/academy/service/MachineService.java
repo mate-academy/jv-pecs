e7d92a25ca61e0d67c813fc7e7b31abd9d14f972
Anton Kurylo <antonkurilo9@gmail.com>
2023-01-27 01:51:43 +0200

    solved task

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -1,11 +1,12 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
 import java.util.List;
 
 /**
  * Parameterize this service and add its implementation in a separate class.
  */
-public interface MachineService {
+public interface MachineService<T extends Machine> {
 
     /**
      * Return the list of machines.
@@ -13,10 +14,10 @@ public interface MachineService {
      * See that 'Class type' is not parametrized.
      * Consider to parametrize this Class< PARAMETRIZE_ME > with specific wildcard bounding
      *
-     * @param type - any class of Machine sub class. For example: Truck.class or Bulldozer.class
+     * @param type - any class of Machine subclass. For example: Truck.class or Bulldozer.class
      * @return the list of machines
      */
-    List<Object> getAll(Class type);
+    List<T> getAll(Class<? extends T> type);
 
     /**
      * Fill the machines list with passed value
@@ -24,9 +25,9 @@ public interface MachineService {
      * This method should be able to work well with any type of machine passed as 'value'
      *
      * @param machines - list of machines to be filled with value
-     * @param value    - any object of machine sub class
+     * @param value    - any object of machine subclass
      */
-    void fill(List<Object> machines, Object value);
+    void fill(List<? super T> machines, Machine value);
 
     /**
      * Call the method doWork() from each machine.
@@ -34,5 +35,5 @@ public interface MachineService {
      *
      * @param machines - the list of machines
      */
-    void startWorking(List<Object> machines);
+    void startWorking(List<? extends T> machines);
 }
