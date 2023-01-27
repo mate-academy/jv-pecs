fc1bcc2582d79d07f97ce0e234c86c88ce5d1a05
Kateryna <katerynachi5@gmail.com>
2023-01-27 17:18:42 +0100

    Make changes after the code review

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -1,16 +1,14 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
-        List<Bulldozer> bulldozers = new ArrayList<>();
-        bulldozers.add(new Bulldozer("Track type 1", "Cab type 1", 100));
-        bulldozers.add(new Bulldozer("Track type 2", "Cab type 2", 200));
-        bulldozers.add(new Bulldozer("Track type 3", "Cab type 3", 300));
-        return bulldozers;
+        return List.of(
+                new Bulldozer("Track type 1", "Cab type 1", 100),
+                new Bulldozer("Track type 2", "Cab type 2", 200),
+                new Bulldozer("Track type 3", "Cab type 3", 300));
     }
 }
--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,16 +1,14 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
     @Override
     public List<Excavator> get() {
-        List<Excavator> excavators = new ArrayList<>();
-        excavators.add(new Excavator("Boom type 1", 5, 2));
-        excavators.add(new Excavator("Boom type 2", 10, 5));
-        excavators.add(new Excavator("Boom type 3", 15, 7));
-        return excavators;
+        return List.of(
+                new Excavator("Boom type 1", 5, 2),
+                new Excavator("Boom type 2", 10, 5),
+                new Excavator("Boom type 3", 15, 7));
     }
 }
