27bbc1543c3ba4c66b9fe151232ffb6c1f2a5108
mitresko <mitresko12@gmail.com>
2023-01-30 17:35:40 +0200

    change signature of methods to compile

---
+++ src/main/java/core/mate/academy/service/BuldozerProducer.java
@@ -0,0 +1,12 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+
+import java.util.List;
+
+public class BuldozerProducer implements MachineProducer{
+    @Override
+    public List<Bulldozer> get() {
+        return null;
+    }
+}
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,25 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public List<Machine> getAll(Class type) {
+        return null;
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+
+    }
 }
