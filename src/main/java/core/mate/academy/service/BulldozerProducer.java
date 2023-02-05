c1ac83ccbaaecadb69efef22326acd628b280de4
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-02-05 23:26:15 +0200

    realization Pecs

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -3,8 +3,8 @@ package core.mate.academy.service;
 import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Machine;
 
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BulldozerProducer implements MachineProducer<Bulldozer> {
