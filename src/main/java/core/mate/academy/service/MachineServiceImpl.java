066d6640d356e5d80c9005bc079545e879acadcf
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-02-07 22:48:31 +0200

    fixed realization Pecs

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -8,27 +8,34 @@ import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
+    private final MachineProducer<Bulldozer> bulldozerMachineProducer = new BulldozerProducer();
+    private final MachineProducer<Excavator> excavatorMachineProducer = new ExcavatorProducer();
+    private final MachineProducer<Truck> truckMachineProducer = new TruckProducer();
+
     @Override
     public List<? extends Machine> getAll(Class type) {
+
         if (type == Bulldozer.class) {
-            return new BulldozerProducer().get();
+            return bulldozerMachineProducer.get();
         }
         if (type == Excavator.class) {
-            return new ExcavatorProducer().get();
+            return excavatorMachineProducer.get();
         }
         if (type == Truck.class) {
-            return new TruckProducer().get();
+            return truckMachineProducer.get();
         }
         return Collections.emptyList();
     }
 
     @Override
     public void startWorking(List<? extends Machine> list) {
-
+        for (Machine machine : list) {
+            machine.doWork();
+        }
     }
 
     @Override
-    public void fill(List machines, Machine value) {
+    public void fill(List<? super Machine> machines, Machine value) {
         Collections.fill(machines, value);
     }
 }
