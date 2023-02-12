3bef917a4d44aa3e771879e6559bfe9ab9976599
WallRun <dimonty2003@gmail.com>
2023-02-12 11:55:30 +0200

    Fixed some type errors and extracted class variables

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -9,15 +9,19 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
+    private static final MachineProducer<Bulldozer> BULLDOZER_PRODUCER = new BulldozerProducer();
+    private static final MachineProducer<Excavator> EXCAVATOR_PRODUCER = new ExcavatorProducer();
+    private static final MachineProducer<Truck> TRUCK_PRODUCER = new TruckProducer();
+
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         MachineProducer<? extends Machine> producer;
         if (type == Bulldozer.class) {
-            producer = new BulldozerProducer();
+            producer = BULLDOZER_PRODUCER;
         } else if (type == Excavator.class) {
-            producer = new ExcavatorProducer();
+            producer = EXCAVATOR_PRODUCER;
         } else if (type == Truck.class) {
-            producer = new TruckProducer();
+            producer = TRUCK_PRODUCER;
         } else {
             return new LinkedList<>();
         }
