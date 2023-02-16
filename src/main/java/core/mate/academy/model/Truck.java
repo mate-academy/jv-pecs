827b596a19060c0c3ee237e61a23137158202f63
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-16 23:27:06 +0200

    MashineService was implemented

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,5 +1,8 @@
 package core.mate.academy.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Add some custom fields that could be only in Truck
  * Do not remove no-field constructor
@@ -11,5 +14,14 @@ public class Truck extends Machine {
     @Override
     public void doWork() {
         System.out.println("Truck started to work");
+    }
+
+    @Override
+    public List<Truck> get() {
+        List<Truck> trucks = new ArrayList<>();
+        trucks.add(new Truck());
+        trucks.add(new Truck());
+        trucks.add(new Truck());
+        return trucks;
     }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,41 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> machines = new ArrayList<>();
+        if (Bulldozer.class.equals(type)) {
+            machines = new Bulldozer().get();
+        } else if (Excavator.class.equals(type)) {
+            machines = new Excavator().get();
+        } else if (Truck.class.equals(type)) {
+            machines = new Truck().get();
+        } else {
+            machines = new ArrayList<Machine>();
+        }
+        return (List<Machine>) machines;
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        int number = machines.size();
+        for (int i = 0; i < number; i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+
+    }
 }
