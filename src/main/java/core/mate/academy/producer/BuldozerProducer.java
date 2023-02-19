506ed48663715d88a17930fc5401b2bb7de74ff4
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-02-19 20:25:56 +0200

    some errors fixed

--- src/main/java/core/mate/academy/producer/BuldozerProducer.java
+++ src/main/java/core/mate/academy/producer/BuldozerProducer.java
@@ -1,17 +1,16 @@
 package core.mate.academy.producer;
 
 import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Machine;
 import core.mate.academy.service.MachineProducer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BuldozerProducer implements MachineProducer {
+public class BuldozerProducer implements MachineProducer<Bulldozer> {
     @Override
-    public List<? extends Machine> get() {
-        List<Bulldozer> machines = new ArrayList();
-        machines.add(new Bulldozer("LoveDozer", "red", 3, 5));
-        machines.add(new Bulldozer("DarkKnight", "black", 1, 3));
-        return machines;
+    public List<Bulldozer> get() {
+        List<Bulldozer> bulldozers = new ArrayList<>();
+        bulldozers.add(new Bulldozer("LoveDozer", "red", 3, 5));
+        bulldozers.add(new Bulldozer("DarkKnight", "black", 1, 3));
+        return bulldozers;
     }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -12,31 +12,37 @@ import java.util.Random;
  * Your implementation of MachineService.
  */
 public class MachineServiceImpl implements MachineService<Machine> {
+    private static final int MAX_COUNT_GENERATING_MACHINE = 100;
 
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
-        List<Machine> machines = new ArrayList();
-        int randomSize = new Random().nextInt(100);
-        if (type == Truck.class) {
-            for (int i = 0; i < randomSize; i++) {
-                machines.add(new Truck());
-            }
-        }
-        if (type == Bulldozer.class) {
-            for (int i = 0; i < randomSize; i++) {
-                machines.add(new Bulldozer());
-            }
-        }
-        if (type == Excavator.class) {
-            for (int i = 0; i < randomSize; i++) {
-                machines.add(new Excavator());
-            }
+        List<Machine> machines = new ArrayList<>();
+        int randomSize = new Random().nextInt(MAX_COUNT_GENERATING_MACHINE);
+        final String typeSimpleName = type.getSimpleName();
+        switch (typeSimpleName) {
+            case "Truck":
+                for (int i = 0; i < randomSize; i++) {
+                    machines.add(new Truck());
+                }
+            break;
+            case "Bulldozer":
+                for (int i = 0; i < randomSize; i++) {
+                    machines.add(new Bulldozer());
+                }
+            break;
+            case "Excavator":
+                for (int i = 0; i < randomSize; i++) {
+                    machines.add(new Excavator());
+                }
+            break;
+            default:
+            break;
         }
         return machines;
     }
 
     @Override
-    public <T> void fill(List<? super T> machines, T value) {
+    public void fill(List<? super Machine> machines, Machine value) {
         int size = machines.size();
         for (int i = 0; i < size; i++) {
             machines.set(i, value);
