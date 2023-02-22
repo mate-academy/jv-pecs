ab6a9ed8deaa591f3e1c2ea1fafce1a6bf1ed298
Viacheslav Kyslyi <kyslyiviacheslav@gmail.com>
2023-02-22 17:15:42 +0200

    Solved task implemented all producer class and method in MachineServiceImpl

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,11 +1,13 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer {
+public interface MachineProducer<T extends Machine> {
+    int MACHINE_NUMBER_FOR_CREATE = 3;
     /**
      * In your implementations - create 2-3 models, add them to the list and return
      * @return - the list of models
      */
-    List<Object> get();
+    List<T> get();
 }
