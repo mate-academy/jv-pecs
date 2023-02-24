76f5479d931600757fc372bd10f79e81e52d2106
Ilko Good <ilko.engineering@gmail.com>
2023-02-24 20:26:03 +0200

    Hell of raw class usage implemented!

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,25 +1,28 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+    private final List<MachineProducer> machineProducers = List.of(
+            new BulldozerProducer(), new ExcavatorProducer(), new TruckProducer());
+    private final List<Class> classes = List.of(
+            Bulldozer.class, Excavator.class, Truck.class);
 
     @Override
     public List<T> getAll(Class<? extends Machine> type) {
-        String classType = type.toString()
-                .substring(type.toString().lastIndexOf(".") + 1);
-        switch (classType) {
-            case "Bulldozer":
-                return (List<T>) new BulldozerProducer().get();
-            case "Excavator":
-                return (List<T>) new ExcavatorProducer().get();
-            case "Truck":
-                return (List<T>) new TruckProducer().get();
-            default:
-                return Collections.emptyList();
+        List<? extends Machine> machines = new ArrayList<>();
+        for (int i = 0; i < classes.size(); i++) {
+            if (classes.get(i) == type) {
+                machines = machineProducers.get(i).get();
+            }
         }
+        return (List<T>) new ArrayList<>(machines);
     }
 
     @Override
