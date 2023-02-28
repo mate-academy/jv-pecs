078b81e223de500cdf3ca6547ff05ff6a073ca61
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-02-28 18:25:06 +0200

    Added limit in generic of interfaces.

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,8 +1,9 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer<M> {
+public interface MachineProducer<M extends Machine> {
     int MACHINE_QUANTITY = 3;
 
     List<M> get();
