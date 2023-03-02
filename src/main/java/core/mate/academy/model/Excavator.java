03f37a2f0358faa168816051949fb571719c4860
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-03-02 14:00:15 +0200

    fixed naming

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,13 +5,13 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
-    private int cabCapacity;
+    private int cabinCapacity;
 
     public Excavator() {
     }
 
-    public Excavator(int cabCapacity) {
-        this.cabCapacity = cabCapacity;
+    public Excavator(int cabinCapacity) {
+        this.cabinCapacity = cabinCapacity;
     }
 
     @Override
