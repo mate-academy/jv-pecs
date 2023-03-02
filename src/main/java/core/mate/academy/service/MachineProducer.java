f50df4e13f7266a8c9167b2a878e72f3f989f9ab
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-03-02 22:16:02 +0200

    check style fix

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -2,6 +2,7 @@ package core.mate.academy.service;
 
 import core.mate.academy.model.Machine;
 import java.util.List;
+
 public interface MachineProducer<T extends Machine> {
     int MACHINE_QUANTITY = 3;
     List<T> get();
