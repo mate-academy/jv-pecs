43fbd1f7db02592cc7106079b5b525441792657e
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-03-02 22:11:21 +0200

    MachineProducer<T extends Machine>

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,8 +1,10 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+
 import java.util.List;
 
-public interface MachineProducer<T> {
+public interface MachineProducer<T extends Machine> {
     int MACHINE_QUANTITY = 3;
     List<T> get();
 }
