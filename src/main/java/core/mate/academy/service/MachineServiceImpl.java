57bfd6bea8d8c231c05d1d5761f36d6d48d6ada5
Nikita Melnikov <bam19921303@gmail.com>
2023-03-13 12:46:49 +0200

    All tasks done -Created fields in Bulldozer, Truck, Excavator models and their getters and setters -Parameterized interfaces MachineProducer and MachineService -Implemented classes BulldozerProducer, ExcavatorProducer, TruckProducer

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,45 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<M extends Machine> implements MachineService<M> {
+    private MachineProducer<Bulldozer> bulldozerProducer = new BulldozerProducer();
+    private MachineProducer<Excavator> excavatorProducer = new ExcavatorProducer();
+    private MachineProducer<Truck> truckProducer = new TruckProducer();
+
+    @Override
+    public List getAll(Class type) {
+        if (type.equals(Bulldozer.class)) {
+            return new ArrayList<>(bulldozerProducer.get());
+        } else if (type.equals(Excavator.class)) {
+            return new ArrayList<>(excavatorProducer.get());
+        } else if (type.equals(Truck.class)) {
+            return new ArrayList<>(truckProducer.get());
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void fill(List<? super M> machines, M value) {
+        int sizeList = machines.size();
+        for (int i = 0; i < sizeList; i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends M> machines) {
+        for (M machine: machines) {
+            machine.doWork();
+        }
+    }
 }
