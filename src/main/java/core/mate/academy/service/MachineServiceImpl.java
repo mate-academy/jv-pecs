fd4fc3554277a565398937c4fd0b4f3848a6bcc8
Oles <a.vykhovanok@gmail.com>
2023-04-08 19:54:56 +0300

    solevd task

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,39 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.Workable;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> resultList = Collections.emptyList();
+        if (type == Bulldozer.class) {
+            resultList = new BulldozerProducer().get();
+        }
+        if (type == Excavator.class) {
+            resultList = new ExcavatorProducer().get();
+        }
+        if (type == Truck.class) {
+            resultList = new TruckProducer().get();
+        }
+        return (List<Machine>) resultList;
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        Collections.fill(machines, value);
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> list) {
+        list.forEach(Workable::doWork);
+    }
 }
