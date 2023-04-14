1189861164c9e32156589af9d6d110cb63db83f6
Serhii Nosulia <sergeynosylya@gmail.com>
2023-04-14 14:05:24 +0300

    added solution

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,51 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String manufacturer;
+    private String model;
+    private int yearOfManufacture;
+    private String fuelType;
+
+    public Excavator(String manufacturer, String model, int yearOfManufacture, String fuelType) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+        this.fuelType = fuelType;
+    }
+
     public Excavator() {
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
     }
 
     @Override
