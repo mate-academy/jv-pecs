46ea1adf28b99370baf7d148ec444e008e6833d6
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-04-15 17:44:05 +0200

    all tests passed

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,31 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int loadCapacity;
+    private int size;
+
     public Truck() {
+    }
+
+    public Truck(int loadCapacity, int size) {
+        this.loadCapacity = loadCapacity;
+        this.size = size;
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     @Override
