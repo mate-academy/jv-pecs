f8a60311fdd3683e237b84e46ecac73b605e14d6
Anton Myronov <mironovanton5511@gmail.com>
2023-04-24 15:50:54 +0300

    removed empty lines, parameterized MachineProducer

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BulldozerProducer implements MachineProducer<Bulldozer> {
-
     @Override
     public List<Bulldozer> get() {
         List<Bulldozer> bulldozers = new ArrayList<>();
