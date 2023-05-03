1bfaefd637bbdc152ea8a57117fb59e5b2504a9f
Oleh Pryshchepa <poleja122@gmail.com>
2023-05-03 12:13:49 +0300

    fixed misstakes

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,9 +1,6 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,19 +12,19 @@ public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         MachineProducer<? extends Machine> machineProducer;
-        List<? extends Machine> machines = new ArrayList<>();
-        if (Bulldozer.class.isAssignableFrom(type)) {
-            machineProducer = new BulldozerProducer();
-            machines = machineProducer.get();
-        } else if (Excavator.class.isAssignableFrom(type)) {
-            machineProducer = new ExcavatorProducer();
-            machines = machineProducer.get();
-        } else if (Truck.class.isAssignableFrom(type)) {
-            machineProducer = new TruckProducer();
-            machines = machineProducer.get();
+        switch (type.getSimpleName()) {
+            case "Bulldozer":
+                machineProducer = new BulldozerProducer();
+                return new ArrayList<>(machineProducer.get());
+            case "Excavator":
+                machineProducer = new ExcavatorProducer();
+                return new ArrayList<>(machineProducer.get());
+            case "Truck":
+                machineProducer = new TruckProducer();
+                return new ArrayList<>(machineProducer.get());
+            default:
+                return new ArrayList<>();
         }
-
-        return new ArrayList<>(machines);
     }
 
     @Override
