a80f441e32595ae7308808eb097e7928741b59ce
Serhii Dudko <nechupara@gmail.com>
2023-05-07 23:28:32 +0300

    implemented hw jv-pecs

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -1,13 +1,16 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-
+import core.mate.academy.model.Machine;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer<Bulldozer>{
-    public List<Bulldozer> get() {
-        List<Bulldozer> bulldozers = new ArrayList<>();
+public class BulldozerProducer implements MachineProducer<Machine> {
+    public List<Machine> get() {
+        List<Machine> bulldozers = new ArrayList<>();
+        bulldozers.add(new Bulldozer());
+        bulldozers.add(new Bulldozer());
+        bulldozers.add(new Bulldozer());
         return bulldozers;
-    };
+    }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,10 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,14 +12,31 @@ import java.util.List;
  */
 public class MachineServiceImpl implements MachineService<Machine> {
     public List<Machine> getAll(Class type) {
-        return null;
+        if (type == Bulldozer.class) {
+            return new BulldozerProducer().get();
+        }
+        if (type == Excavator.class) {
+            return new ExcavatorProducer().get();
+        }
+        if (type == Truck.class) {
+            return new TruckProducer().get();
+        } else {
+            return new ArrayList<>();
+        }
+
     }
 
     public void fill(List<? super Machine> machines, Machine value) {
-
+        int size = machines.size();
+        machines.clear();
+        for (int i = 0; i < size; i++) {
+            machines.add(i, value);
+        }
     }
 
     public void startWorking(List<? extends Machine> machines) {
-
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
     }
 }
