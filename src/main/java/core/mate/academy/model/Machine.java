396ea1aedd1d724420c08d89f7f4391523c2d100
Ihor Fetov <92263491+Gamemod13@users.noreply.github.com>
2023-05-10 11:25:37 +0300

    Update src/main/java/core/mate/academy/model/Machine.java
    Co-authored-by: Artur Sarakhman <53354844+sarakhmen@users.noreply.github.com>

--- src/main/java/core/mate/academy/model/Machine.java
+++ src/main/java/core/mate/academy/model/Machine.java
@@ -1,8 +1,8 @@
 package core.mate.academy.model;
 
 public abstract class Machine implements Workable {
-    public static final String DEFAULT_NAME = "Pussy_waggon";
-    public static final String DEFAULT_COLOUR = "White";
+    private static final String DEFAULT_NAME = "Pussy_waggon";
+    private static final String DEFAULT_COLOUR = "White";
     private String name;
     private String color;
 
