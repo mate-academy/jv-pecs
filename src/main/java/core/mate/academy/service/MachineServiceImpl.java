b488caaebb5ed2b659e3a5b74a002da79584ae5c
Ros <rospsix@gmail.com>
2023-05-11 16:37:16 +0100

    #7
    "I created 3 final variables with the necessary values that will be used later in switch statment.
    
    In the getAll() method, i initially wanted to use an if-else statement, but decided that a switch would be easier to understand. This code determines which type of machine is passed to getAll() using the type argument, and then an object of the corresponding class is created. If the passed type does not match, an empty list is returned.
    
    the fill method populates the machines list with the value, using an upper bounded wildcard <? super Machine>, which allows objects of type Machine and its superclasses to be added to the list.
    
     And finally, the startWorking() method starts the operation for each machine in the list.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,49 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.BulldozerProducer;
+import core.mate.academy.model.ExcavatorProducer;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.TruckProducer;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    private static final String BULLDOZER = "Bulldozer";
+    private static final String EXCAVATOR = "Excavator";
+    private static final String TRUCK = "Truck";
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        MachineProducer<? extends Machine> machineProducer;
+        switch (type.getSimpleName()) {
+            case BULLDOZER:
+                machineProducer = new BulldozerProducer();
+                return new ArrayList<>(machineProducer.get());
+            case EXCAVATOR:
+                machineProducer = new ExcavatorProducer();
+                return new ArrayList<>(machineProducer.get());
+            case TRUCK:
+                machineProducer = new TruckProducer();
+                return new ArrayList<>(machineProducer.get());
+            default:
+                return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
