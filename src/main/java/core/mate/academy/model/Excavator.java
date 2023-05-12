26116121a79f56a7b059be27c21e5caa4f277aec
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 10:42:23 +0300

    implements methods

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,13 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String model;
+
     public Excavator() {
+    }
+
+    public Excavator(String model) {
+        this.model = model;
     }
 
     @Override
