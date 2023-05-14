23e9bd837ba8ee3a8326d79968d79fdf9d1f86ce
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-05-14 19:21:36 +0200

    if-else block changed on switch case

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,24 +1,22 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
-        MachineProducer<? extends Machine> producer = null;
-        if (type == Bulldozer.class) {
-            producer = new BulldozerProducer();
-        } else if (type == Excavator.class) {
-            producer = new ExcavatorProducer();
-        } else if (type == Truck.class) {
-            producer = new TruckProducer();
+        switch (type.getSimpleName()) {
+            case "Truck":
+                return new ArrayList<>(new TruckProducer().get());
+            case "Excavator":
+                return new ArrayList<>(new ExcavatorProducer().get());
+            case "Bulldozer":
+                return new ArrayList<>(new BulldozerProducer().get());
+            default:
+                return new ArrayList<>();
         }
-        return producer != null ? new ArrayList<>(producer.get()) : new ArrayList<>();
     }
 
     @Override
