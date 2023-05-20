3db7db1b757ec5ae690eef0d17610213f0c9bcda
Stipaxa <petrivs572@gmail.com>
2023-05-20 17:33:33 +0300

    Add some fields to classes: Bulldozer, Excavator, Truck. Parametrize MachineProducer class and create 3 implementations this interface: BulldozerProducer, ExcavatorProducer, TruckProducer. Parametrize MachineService interface and implement it in MachineServiceImpl class.

---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,19 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Truck> {
+    @Override
+    public List<Truck> get() {
+        return createTrucks();
+    }
+
+    private List<Truck> createTrucks() {
+        List<Truck> trucks = new ArrayList<>();
+        trucks.add(new Truck("Truck 1", "Black", 14000));
+        trucks.add(new Truck("Truck 2", "White", 14500));
+        return trucks;
+    }
+}
