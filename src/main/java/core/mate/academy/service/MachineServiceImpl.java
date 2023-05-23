4ba8ad56bf39f9678af0871d6ca89c7c6f8a8a2a
Stipaxa <petrivs572@gmail.com>
2023-05-23 20:26:46 +0300

    Fixed startWorking method in MachineService. Removed empty line in MachineServiceImpl

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -12,7 +12,6 @@ import java.util.List;
  * Your implementation of MachineService.
  */
 public class MachineServiceImpl implements MachineService<Machine> {
-
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type.equals(Bulldozer.class)) {
