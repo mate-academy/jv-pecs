35c70c9bb24b47a46ae1f38c34ba7743d35c0f6a
Oleh Sarapuk <olehsarapuk@gmail.com>
2023-06-14 12:03:44 +0300

    solution

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,45 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.Collections;
+import java.util.List;
+
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+    private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
+    private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+    private final TruckProducer truckProducer = new TruckProducer();
+
+    @Override
+    public List<T> getAll(Class<? extends Machine> type) {
+        if (type.equals(Bulldozer.class)) {
+            return (List<T>) bulldozerProducer.get();
+        }
+        if (type.equals(Excavator.class)) {
+            return (List<T>) excavatorProducer.get();
+        }
+        if (type.equals(Truck.class)) {
+            return (List<T>) truckProducer.get();
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        if (machines == null) {
+            throw new RuntimeException("No machines in list");
+        }
+        for (Machine m : machines) {
+            m.doWork();
+        }
+    }
 }
