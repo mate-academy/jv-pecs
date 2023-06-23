2d992a9b9a7e58c9883448b6360b8b9afb98846e
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2023-06-23 22:02:48 +0300

    completed task

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,7 +5,32 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private float bucketCapacity;
+    private String trackType;
+
     public Bulldozer() {
+
+    }
+
+    public Bulldozer(float bucketCapacity, String trackType) {
+        this.bucketCapacity = bucketCapacity;
+        this.trackType = trackType;
+    }
+
+    public float getBucketCapacity() {
+        return bucketCapacity;
+    }
+
+    public void setBucketCapacity(float bucketCapacity) {
+        this.bucketCapacity = bucketCapacity;
+    }
+
+    public String getTrackType() {
+        return trackType;
+    }
+
+    public void setTrackType(String trackType) {
+        this.trackType = trackType;
     }
 
     @Override
--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,32 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private float maxDiggingDepth;
+    private String bucketType;
+
     public Excavator() {
+
+    }
+
+    public Excavator(float maxDiggingDepth, String bucketType) {
+        this.bucketType = bucketType;
+        this.maxDiggingDepth = maxDiggingDepth;
+    }
+
+    public float getMaxDiggingDepth() {
+        return maxDiggingDepth;
+    }
+
+    public void setMaxDiggingDepth(float maxDiggingDepth) {
+        this.maxDiggingDepth = maxDiggingDepth;
+    }
+
+    public String getBucketType() {
+        return bucketType;
+    }
+
+    public void setBucketType(String bucketType) {
+        this.bucketType = bucketType;
     }
 
     @Override
