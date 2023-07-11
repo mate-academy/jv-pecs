eb10d7120e38531db7a609a2e37309d8c186099f
Denis Moroz <moroz06denis@gmail.com>
2023-07-11 19:55:09 +0200

    implemented task

--- src/main/java/core/mate/academy/model/Machine.java
+++ src/main/java/core/mate/academy/model/Machine.java
@@ -1,6 +1,6 @@
 package core.mate.academy.model;
 
-public abstract class Machine implements Workable {
+public abstract class imMachine implements Workable {
     private String name;
     private String color;
 
