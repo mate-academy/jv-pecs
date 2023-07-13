b46513227a9f14afaf92719ddee99f1018da13aa
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-07-13 23:51:05 +0300

    Update BulldozerProducer.java

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -1,13 +1,12 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class BulldozerProducer implements MachineProducer<Bulldozer> {
-    Bulldozer firstBulldozer = new Bulldozer(90, 40);
-    Bulldozer secondBulldozer = new Bulldozer(80, 50);
+    private Bulldozer firstBulldozer = new Bulldozer(90, 40);
+    private Bulldozer secondBulldozer = new Bulldozer(80, 50);
 
     @Override
     public List<Bulldozer> get() {
