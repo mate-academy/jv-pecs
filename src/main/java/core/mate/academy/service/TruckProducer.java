6f9ef17b2f87a5c2001314204c2a06f8fc63a526
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-07-13 23:53:42 +0300

    Update TruckProducer.java

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,14 +1,12 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Truck;
-import core.mate.academy.model.Truck;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
-    Truck firstTruck = new Truck(2, 145);
-    Truck secondTruck = new Truck(4, 255);
+    private Truck firstTruck = new Truck(2, 145);
+    private Truck secondTruck = new Truck(4, 255);
 
     @Override
     public List<Truck> get() {
