aa6b28c60203f83b844c1c8661b64525a3a6a034
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-07-15 17:51:35 +0300

    deleted loop from getAll method

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -15,14 +15,12 @@ public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         List<Machine> machineList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            if (type.equals(Bulldozer.class)) {
-                machineList = new ArrayList<>(new BulldozerProducer().get());
-            } else if (type.equals(Excavator.class)) {
-                machineList = new ArrayList<>(new ExcavatorProducer().get());
-            } else if (type.equals(Truck.class)) {
-                machineList = new ArrayList<>(new TruckProducer().get());
-            }
+        if (type.equals(Bulldozer.class)) {
+            machineList = new ArrayList<>(new BulldozerProducer().get());
+        } else if (type.equals(Excavator.class)) {
+            machineList = new ArrayList<>(new ExcavatorProducer().get());
+        } else if (type.equals(Truck.class)) {
+            machineList = new ArrayList<>(new TruckProducer().get());
         }
         return machineList;
     }
