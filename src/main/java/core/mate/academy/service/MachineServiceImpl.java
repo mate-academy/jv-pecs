f3bb716e53c08ff4edfd7cf360515d9a03839b6f
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-15 20:50:35 +0300

    removed reduntant parameter

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,33 +5,31 @@ import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<Machine> {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
     @Override
-    public List<Machine> getAll(Class<? extends Machine> type) {
-        List<Machine> machineList = new ArrayList<>();
+    public List<T> getAll(Class<? extends T> type) {
+        List<T> machineList = new ArrayList<>();
         if (type.equals(Truck.class)) {
-            machineList = new ArrayList<>(new TruckProducer().get());
+            machineList = (List<T>) new ArrayList<>(new TruckProducer().get());
         } else if (type.equals(Bulldozer.class)) {
-            machineList = new ArrayList<>(new BulldozerProducer().get());
+            machineList = (List<T>) new ArrayList<>(new BulldozerProducer().get());
         } else if (type.equals(Excavator.class)) {
-            machineList = new ArrayList<>(new ExcavatorProducer().get());
+            machineList = (List<T>) new ArrayList<>(new ExcavatorProducer().get());
         }
         return machineList;
     }
 
     @Override
-    public void fill(List<? super Machine> list, Machine value) {
+    public void fill(List<? super T> list, T value) {
         for (int i = 0; i < list.size(); i++) {
             list.set(i, value);
         }
     }
 
-
     @Override
-    public void startWorking(List<? extends Machine> machines) {
+    public void startWorking(List<? extends T> machines) {
         for (Machine machine : machines) {
             machine.doWork();
         }
