f66e2ba214571e34b7d37d243d1404b06ac78a2e
Stanislav Golubkov <golubkovstas83@gmail.com>
2023-07-16 22:19:53 +0300

    version2-stas (fixed bugs)

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,7 +4,6 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +20,7 @@ public class MachineServiceImpl<T extends Machine> implements MachineService<T> 
 
     @Override
     public List<T> getAll(Class<? extends T> type) {
-        if(type == Bulldozer.class) {
+        if (type == Bulldozer.class) {
             return (List<T>) new ArrayList<>(bulldozerMachineProducer.get());
         } else if (type == Excavator.class) {
             return (List<T>) new ArrayList<>(excavatorMachineProducer.get());
