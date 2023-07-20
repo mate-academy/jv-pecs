a7a100d30e1ba88da47096624832ac7398be943f
Vitalii <stelmaschyk.vg@gmail.com>
2023-07-20 12:43:01 +0300

    Mentor's comments were implemented

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,21 +1,11 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
-    private Excavator excavator1 = new Excavator();
-    private Excavator excavator2 = new Excavator();
-    private Excavator excavator3 = new Excavator();
-    private List<Excavator> excavators = List.of(excavator1, excavator2, excavator3);
-
     @Override
     public List<Excavator> get() {
-        List<Excavator> listOfExcavators = new ArrayList<Excavator>();
-        for (Object element : excavators) {
-            listOfExcavators.add((Excavator) element);
-        }
-        return listOfExcavators;
+        return List.of(new Excavator(), new Excavator(), new Excavator());
     }
 }
--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,22 +1,11 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Truck;
-import java.util.ArrayList;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
-    private Truck truck1 = new Truck();
-    private Truck truck2 = new Truck();
-    private Truck truck3 = new Truck();
-    private List<Truck> trucks = List.of(truck1, truck2, truck3);
-
     @Override
     public List<Truck> get() {
-        List<Truck> listOfTrucks = new ArrayList<>();
-        for (Object element : trucks) {
-            listOfTrucks.add((Truck) element);
-        }
-        return listOfTrucks;
+        return List.of(new Truck(), new Truck(), new Truck());
     }
 }
-
