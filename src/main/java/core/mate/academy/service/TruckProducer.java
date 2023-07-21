dd52d2431a1f67967541da81fa4c294df85075d1
Влад <dakadofordisluv@gmail.com>
2023-07-21 23:38:50 +0300

    fixed after review

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,15 +1,18 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Truck;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
-    private static final Truck first = new Truck();
-    private static final Truck second = new Truck();
-    private static final Truck third = new Truck();
+    private static final int PRODUCTION_AMOUNT = 3;
 
     @Override
     public List<Truck> get() {
-        return List.of(first, second, third);
+        List<Truck> consignment = new ArrayList<>();
+        for (int i = PRODUCTION_AMOUNT; i > 0; i--) {
+            consignment.add(new Truck());
+        }
+        return consignment;
     }
 }
