87094a21a4604620bdbd55cdc3934d3b2526f936
Olga Starkova <ole4kastar79@gmail.com>
2023-08-03 23:51:02 +0300

    pecs My MachineServiceImpl fixed build

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -3,7 +3,7 @@ package core.mate.academy.service;
 import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer<Machines> {
+public interface MachineProducer<Machine> {
     /**
      * In your implementations - create 2-3 models, add them to the list and return
      *
