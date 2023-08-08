7c239947bf942f33748d2071cdebe25f0d737ff8
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-08-08 21:55:40 +0300

    hw-producer-extends-consumer-super

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,12 +5,13 @@ import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MachineServiceImpl implements MachineService<Machine> {
-    private MachineProducer<Truck> truckMachineProducer = new TruckProducer();
-    private MachineProducer<Excavator> excavatorMachineProducer = new ExcavatorProducer();
-    private MachineProducer<Bulldozer> bulldozerMachineProducer = new BulldozerProducer();
+    private final MachineProducer<Truck> truckMachineProducer = new TruckProducer();
+    private final MachineProducer<Excavator> excavatorMachineProducer = new ExcavatorProducer();
+    private final MachineProducer<Bulldozer> bulldozerMachineProducer = new BulldozerProducer();
 
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
@@ -26,9 +27,7 @@ public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
     public void fill(List<? super Machine> machines, Machine value) {
-        for (int i = 0; i < machines.size(); i++) {
-            machines.set(i, value);
-        }
+        Collections.fill(machines, value);
 
     }
 
