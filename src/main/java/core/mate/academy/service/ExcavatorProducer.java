bade3221a5e30890a141f930259d2626c8c866b8
Anna Shokun <anne.shokun@gmail.com>
2023-08-09 14:59:17 +0300

    homework solution

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,15 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        excavators.add(new Excavator("Excavator1", "Green", 200, true));
+        excavators.add(new Excavator("Excavator2", "Grey", 250, false));
+        return excavators;
+    }
+}
