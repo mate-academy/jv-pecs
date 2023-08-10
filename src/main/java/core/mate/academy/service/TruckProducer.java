88144bf337cae4c48530237cc24a1df0c7f2aad1
OSlovachevska <slovachevska.ok@gmail.com>
2023-08-10 19:17:13 +0300

    added solution after review

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,13 +1,12 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
     @Override
-    public List<? extends Machine> get() {
+    public List<Truck> get() {
         List<Truck> trucks = new ArrayList<>();
         trucks.add(new Truck("green", 2005));
         return trucks;
