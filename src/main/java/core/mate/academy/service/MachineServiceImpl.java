aca22868c163126d601c87597e18ffaef1c524ea
Solomia Bobyak <mia.bobyak@gmail.com>
2023-08-15 13:16:22 +0300

    implemented methods that are inherited from the interface MachineService<Machine>

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,42 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<Machine> machines = new ArrayList<>();
+
+        if (type.equals(Bulldozer.class)) {
+            Bulldozer bulldozer = new Bulldozer();
+            machines.add(bulldozer);
+        } else if (type.equals(Excavator.class)) {
+            Excavator excavator = new Excavator();
+            machines.add(excavator);
+        } else if (type.equals(Truck.class)) {
+            Truck truck = new Truck();
+            machines.add(truck);
+        }
+
+        return machines;
+    }
 }
