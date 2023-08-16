82763bccf80fcc1b0c8cecb9c01db762aadc78e9
Solomia Bobyak <mia.bobyak@gmail.com>
2023-08-16 20:21:13 +0300

    changed mvn error

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -1,12 +1,10 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer<Bulldozer>{
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
         List<Bulldozer> bulldozer = new ArrayList<>();
