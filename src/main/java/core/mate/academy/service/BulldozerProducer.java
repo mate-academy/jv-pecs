37f2179a97320b7b78498b81cdce978a712de315
Solomia Bobyak <mia.bobyak@gmail.com>
2023-08-16 20:13:22 +0300

    created classes that return empty lists of machines

---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer>{
+    @Override
+    public List<Bulldozer> get() {
+        List<Bulldozer> bulldozer = new ArrayList<>();
+        bulldozer.add(new Bulldozer());
+        return bulldozer;
+    }
+}
