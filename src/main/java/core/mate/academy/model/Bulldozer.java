311b743a28312442b73b72ff5dacd3417d384fba
Kolinko Oleksii <alexkolinko22@gmail.com>
2023-08-17 23:58:01 +0200

    HW13-solution

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -4,7 +4,17 @@ package core.mate.academy.model;
  * Add some custom fields that could be only in Bulldozer
  * Do not remove no-args constructor
  */
+
 public class Bulldozer extends Machine {
+    private String brand;
+    private int cost;
+
+    public Bulldozer(String name, String color, String brand, int cost) {
+        super(name, color);
+        this.brand = brand;
+        this.cost = cost;
+    }
+
     public Bulldozer() {
     }
 
--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -4,7 +4,17 @@ package core.mate.academy.model;
  * Add some custom fields that could be only in Excavator
  * Do not remove no-args constructor
  */
+
 public class Excavator extends Machine {
+    private String model;
+    private int weight;
+
+    public Excavator(String name, String color, String model, int weight) {
+        super(name, color);
+        this.model = model;
+        this.weight = weight;
+    }
+
     public Excavator() {
     }
 
