6407653e2ce8529db774414ace2aa0b35ed5d5db
Alex-Solod <solodyhin_2@icloud.com>
2023-08-18 15:57:43 +0300

    Fixed: Using the '.*' form of import should be avoided - core.mate.academy.model.*; Extra separation in import group before 'java.util.ArrayList'; Comment has incorrect indentation level 0, expected is 4, indentation should be the same level as line 33.

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -18,9 +18,8 @@ public interface MachineService<T> {
      * @param type - any class of Machine sub class. For example: Truck.class or Bulldozer.class
      * @return the list of machines
      */
-    List<T> getAll(Class<? extends T> type);
 
-//    List getAll(Class type);
+    List<T> getAll(Class<? extends T> type);
 
     /**
      * Fill the machines list with passed value
@@ -30,6 +29,7 @@ public interface MachineService<T> {
      * @param machines - list of machines to be filled with value
      * @param value    - any object of machine sub class
      */
+
     void fill(List<? super T> machines, T value);
 
     /**
@@ -38,5 +38,6 @@ public interface MachineService<T> {
      *
      * @param machines - the list of machines
      */
+
     void startWorking(List<? extends T> machines);
 }
