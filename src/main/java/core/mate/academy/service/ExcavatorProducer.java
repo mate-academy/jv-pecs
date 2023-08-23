dd92d4726792f6f0a7ad510f24b8bfa13bb82b99
Mykola Mosii <mykolamosii@gmail.com>
2023-08-23 12:47:29 +0300

    Implemented MachineServiceImpl class

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    @Override
+    public List<Excavator> get() {
+        ArrayList<Excavator> machines = new ArrayList<>();
+        machines.add(new Excavator());
+        machines.add(new Excavator());
+        machines.add(new Excavator());
+        return machines;
+    }
+}
