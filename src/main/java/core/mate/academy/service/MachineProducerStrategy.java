9e95580b60ab2addd1c701b5fbef40fb5257dad7
Olena Stankevych <yelenastankevych@gmail.com>
2023-08-23 17:53:44 +0300

    added fixes after review

--- src/main/java/core/mate/academy/service/MachineProducerStrategy.java
+++ src/main/java/core/mate/academy/service/MachineProducerStrategy.java
@@ -1,24 +1,30 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
 import core.mate.academy.service.producerimpl.BulldozerProducerImpl;
 import core.mate.academy.service.producerimpl.ExcavatorProducerImpl;
 import core.mate.academy.service.producerimpl.TruckProducerImpl;
-import java.util.ArrayList;
+import java.util.List;
 
 public class MachineProducerStrategy {
     public MachineProducer<? extends Machine> getMachineProducerStrategy(
             Class<? extends Machine> type) {
-        switch (type.getSimpleName()) {
-            case "Bulldozer":
-                return new BulldozerProducerImpl();
-            case "Excavator":
-                return new ExcavatorProducerImpl();
-            case "Truck":
-                return new TruckProducerImpl();
-            default:
-                return (MachineProducer<Machine>) ArrayList::new;
+        if (type == Bulldozer.class) {
+            return new BulldozerProducerImpl();
+        } else if (type == Truck.class) {
+            return new TruckProducerImpl();
+        } else if (type == Excavator.class) {
+            return new ExcavatorProducerImpl();
+        } else {
+            return new MachineProducer<Machine>() {
+                @Override
+                public List<Machine> get() {
+                    return List.of();
+                }
+            };
         }
     }
-
 }
