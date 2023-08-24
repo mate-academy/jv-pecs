fa3ce17ab1efa57d3b8098fd82fcacead13e3a47
Teract10s <trs.leonov@gmail.com>
2023-08-24 09:38:03 +0300

    improved solution

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -1,12 +1,11 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Machine;
 import java.util.List;
 
 /**
  * Parameterize this service and add its implementation in a separate class.
  */
-public interface MachineService<T extends Machine> {
+public interface MachineService<T> {
 
     /**
      * Return the list of machines.
@@ -17,7 +16,7 @@ public interface MachineService<T extends Machine> {
      * @param type - any class of Machine sub class. For example: Truck.class or Bulldozer.class
      * @return the list of machines
      */
-    List<Machine> getAll(Class type);
+    List<T> getAll(Class<? extends T> type);
 
     /**
      * Fill the machines list with passed value
@@ -27,7 +26,7 @@ public interface MachineService<T extends Machine> {
      * @param machines - list of machines to be filled with value
      * @param value    - any object of machine sub class
      */
-    void fill(List<? super Machine> machines, Machine value);
+    void fill(List<? super T> machines, T value);
 
     /**
      * Call the method doWork() from each machine.
@@ -35,5 +34,5 @@ public interface MachineService<T extends Machine> {
      *
      * @param machines - the list of machines
      */
-    void startWorking(List<? extends Machine> machines);
+    void startWorking(List<? extends T> machines);
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -10,13 +10,13 @@ import java.util.List;
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl implements MachineService<Machine> {
-    private static final BulldozerProducer BULLDOZER_PRODUCER = new BulldozerProducer();
+public class MachineServiceImpl<T> implements MachineService<T> {
+    private static final MachineProducer BULLDOZER_PRODUCER = new BulldozerProducer();
     private static final ExcavatorProducer EXCAVATOR_PRODUCER = new ExcavatorProducer();
     private static final TruckProducer TRUCK_PRODUCER = new TruckProducer();
 
     @Override
-    public List<Machine> getAll(Class type) {
+    public List<T> getAll(Class<? extends T> type) {
         List<? extends Machine> result = new ArrayList<>();
         if (type.equals(Bulldozer.class)) {
             result = BULLDOZER_PRODUCER.get();
@@ -25,20 +25,20 @@ public class MachineServiceImpl implements MachineService<Machine> {
         } else if (type.equals(Truck.class)) {
             result = TRUCK_PRODUCER.get();
         }
-        return (List<Machine>) result;
+        return (List<T>) result;
     }
 
     @Override
-    public void fill(List<? super Machine> machines, Machine value) {
+    public void fill(List<? super T> machines, T value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
     }
 
     @Override
-    public void startWorking(List<? extends Machine> machines) {
-        for (Machine machine : machines) {
-            machine.doWork();
+    public void startWorking(List<? extends T> machines) {
+        for (Object machine : machines) {
+            ((Machine) machine).doWork();
         }
     }
 }
