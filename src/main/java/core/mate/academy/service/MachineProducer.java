70c56af48002659df3b6b809ba325d1386b10ced
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-08-26 21:17:59 +0300

    deleted comments

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -6,10 +6,5 @@ import java.util.List;
 public interface MachineProducer<T extends Machine> {
     int AMOUNT_OF_MODELS = 3;
 
-    /**
-     * In your implementations - create 2-3 models, add them to the list and return
-     *
-     * @return - the list of models
-     */
     List<? extends Machine> get();
 }
