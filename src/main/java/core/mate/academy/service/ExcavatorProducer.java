a463c3ce1bf7df8162bd55532e726523fbb6ff07
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-08-29 10:35:02 +0300

    added changes to the class ExcavatorProducer

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,2 +1,16 @@
-package core.mate.academy.service;public class ExcavatorProducer {
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        // Add Excavator objects to the list
+        excavators.add(new Excavator());
+        return excavators;
+    }
 }
