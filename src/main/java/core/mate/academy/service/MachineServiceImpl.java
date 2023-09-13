35c09ef608bc174e564fe5a8269ea73e75a971a9
Andrii Riaboshapka <postagama@gmail.com>
2023-09-14 00:12:54 +0300

    added fixed after review

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -5,24 +5,23 @@ import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-public class MachineServiceImpl<T extends Machine> implements MachineService<Machine> {
-    private List<T> machines = new ArrayList<>();
+public class MachineServiceImpl implements MachineService<Machine> {
+    private List<Machine> machines = new ArrayList<>();
 
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         machines.clear();
         if (type == Bulldozer.class) {
             MachineProducer<Bulldozer> producer = new BulldozerProducer();
-            machines.addAll((Collection<? extends T>) producer.get());
+            machines.addAll(producer.get());
         } else if (type == Excavator.class) {
             MachineProducer<Excavator> producer = new ExcavatorProducer();
-            machines.addAll((Collection<? extends T>) producer.get());
+            machines.addAll(producer.get());
         } else if (type == Truck.class) {
             MachineProducer<Truck> producer = new TruckProducer();
-            machines.addAll((Collection<? extends T>) producer.get());
+            machines.addAll(producer.get());
         }
         return (List<Machine>) machines;
     }
