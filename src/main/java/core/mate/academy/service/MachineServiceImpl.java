d079e13d9ba18b0e63af8974884e897c5d353d29
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-10 21:49:03 +0300

    Implements methods of MachineService interface. Pass tests. Clean up code.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,32 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Machine;
+import core.mate.academy.strategy.MachineProducerStrategy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl implements MachineService<Machine> {
+    private final MachineProducerStrategy machineProducerStrategy = new MachineProducerStrategy();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> machines = machineProducerStrategy
+                .getMachineProducerByMachineType(type.getSimpleName())
+                .get();
+        return new ArrayList<>(machines);
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
