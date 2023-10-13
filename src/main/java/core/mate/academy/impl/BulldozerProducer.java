d7fee64fdd190ceb8ba8088027df6e73460179c4
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-13 11:22:14 +0300

    Add methods and constants from MachineSupplier. For now, we create defaults Machine object in <MachineName>Producer classes.

--- src/main/java/core/mate/academy/impl/BulldozerProducer.java
+++ src/main/java/core/mate/academy/impl/BulldozerProducer.java
@@ -1,22 +1,28 @@
 package core.mate.academy.impl;
 
-import static core.mate.academy.MachineSupplier.DEFAULT_AMOUNT_OF_MACHINE_TO_CREATE;
-
-import core.mate.academy.MachineSupplier;
 import core.mate.academy.model.Bulldozer;
 import core.mate.academy.service.MachineProducer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BulldozerProducer implements MachineProducer<Bulldozer> {
-    private final MachineSupplier machineSupplier = new MachineSupplier();
+    private static final int AMOUNT_OF_MACHINE_TO_CREATE = 3;
+    private static final double BULLDOZER_BLADE_LENGTH = 4500;
+    private static final double BULLDOZER_BLADE_MASS = 15;
+    private static final double BULLDOZER_ROCK_MOVEMENT_DISTANCE = 100;
 
     @Override
     public List<Bulldozer> get() {
         List<Bulldozer> bulldozers = new ArrayList<>();
-        for (int i = 0; i < DEFAULT_AMOUNT_OF_MACHINE_TO_CREATE; i++) {
-            bulldozers.add(machineSupplier.getDefaultBulldozer());
+        for (int i = 0; i < AMOUNT_OF_MACHINE_TO_CREATE; i++) {
+            bulldozers.add(getDefaultBulldozer());
         }
         return bulldozers;
+    }
+
+    private Bulldozer getDefaultBulldozer() {
+        return new Bulldozer(BULLDOZER_BLADE_LENGTH,
+                BULLDOZER_BLADE_MASS,
+                BULLDOZER_ROCK_MOVEMENT_DISTANCE);
     }
 }
