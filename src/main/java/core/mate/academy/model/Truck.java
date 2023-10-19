08a3951383e84cdfbed87d3aa2b29991c2e5c821
almanac1914 <144943174+almanac1914@users.noreply.github.com>
2023-10-19 12:48:33 -0600

    added solution, build success

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,15 +1,29 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Truck
- * Do not remove no-field constructor
- */
 public class Truck extends Machine {
+    private double price;
+    private double engineVolume;
+    private String owner;
+
+    public Truck(double price, double engineVolume, String owner) {
+        this.price = price;
+        this.engineVolume = engineVolume;
+        this.owner = owner;
+    }
+
     public Truck() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Truck started to work");
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
