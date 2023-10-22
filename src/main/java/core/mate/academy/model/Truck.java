ae7d78e2f316666333408ad448b90609e0c0e68f
artificialLeo <horkovenko.k@gmail.com>
2023-10-22 19:42:13 +0300

    done

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,10 +1,7 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Truck
- * Do not remove no-field constructor
- */
 public class Truck extends Machine {
+
     public Truck() {
     }
 
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,43 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.Workable;
+import core.mate.academy.service.producers.BulldozerProducer;
+import core.mate.academy.service.producers.ExcavatorProducer;
+import core.mate.academy.service.producers.TruckProducer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+    @Override
+    public List<Machine> getAll(Class<? extends T> type) {
+        List<Machine> machines = new ArrayList<>();
+
+        if (type == Bulldozer.class) {
+            machines.addAll(new BulldozerProducer().get());
+            return machines;
+        } else if (type == Excavator.class) {
+            machines.addAll(new ExcavatorProducer().get());
+            return machines;
+        } else if (type == Truck.class) {
+            machines.addAll(new TruckProducer().get());
+            return machines;
+        }
+
+        return machines;
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        Collections.fill(machines, value);
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        machines.forEach(Workable::doWork);
+    }
 }
