6e0c4e7b2cc2bf53ea08f4940f1490b058d37fd0
Roman K <romazan07sq@gmail.com>
2023-10-26 12:57:33 +0300

    implement task

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,11 +5,18 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private boolean hasBucket;
+    private boolean hasArm;
+
     public Excavator() {
+    }
+
+    public Excavator(boolean hasBucket, boolean hasArm) {
+        this.hasBucket = hasBucket;
+        this.hasArm = hasArm;
     }
 
     @Override
     public void doWork() {
-        System.out.println("Excavator started to work");
     }
 }
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,15 +1,41 @@
 package core.mate.academy.model;
 
+import core.mate.academy.service.MachineService;
+import java.util.List;
+
 /**
  * Add some custom fields that could be only in Truck
  * Do not remove no-field constructor
  */
-public class Truck extends Machine {
+public class Truck extends Machine implements MachineService {
+    private String modelWheels;
+    private boolean hasBumper;
+
     public Truck() {
+    }
+
+    public Truck(String modelWheels, boolean hasBumper) {
+        this.modelWheels = modelWheels;
+        this.hasBumper = hasBumper;
     }
 
     @Override
     public void doWork() {
-        System.out.println("Truck started to work");
+
+    }
+
+    @Override
+    public List getAll(Class type) {
+        return null;
+    }
+
+    @Override
+    public void fill(List machines, Machine value) {
+
+    }
+
+    @Override
+    public void startWorking(List list) {
+
     }
 }
