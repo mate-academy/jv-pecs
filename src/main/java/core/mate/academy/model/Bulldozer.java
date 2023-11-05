3fd882dc1b85043d94942a1da8ab67ada9684a75
Roman Globa <globaroman@gmail.com>
2023-11-05 17:46:50 +0200

    Implemented methods of MachineerviceImpin accordance with the task condition

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,11 +5,39 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private int enginePower;
+
     public Bulldozer() {
+
+    }
+
+    public Bulldozer(String name, String color, int enginePower) {
+        super.setName(name);
+        super.setColor(color);
+        this.enginePower = enginePower;
     }
 
     @Override
     public void doWork() {
         System.out.println("Bulldozer started to work");
+    }
+
+    @Override
+    public String toString() {
+        return "Bulldozer{name -"
+                + super.getName()
+                + ", color - "
+                + super.getColor()
+                + ", enginePower="
+                + enginePower
+                + '}';
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
     }
 }
