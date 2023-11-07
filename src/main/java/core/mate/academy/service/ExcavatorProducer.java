5c677d8d061140a7efdd682ca45e3f173e84cb38
TkachukTara <zaz42988@gmail.com>
2023-11-07 17:17:40 +0200

    PECS HomeTask Done!

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        excavators.add(new Excavator(200, "Excavator X"));
+        excavators.add(new Excavator(180, "Excavator Y"));
+        return excavators;
+    }
+}
---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,16 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Truck> {
+
+    @Override
+    public List<Truck> get() {
+        List<Truck> trucks = new ArrayList<>();
+        trucks.add(new Truck(400, "Truck 1"));
+        trucks.add(new Truck(350, "Truck 2"));
+        return trucks;
+    }
+}
