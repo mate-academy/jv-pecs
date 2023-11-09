ff9a197d7a54dfc6230365ba367b5820e9eff205
user <graban2253@gmail.com>
2023-11-09 23:47:36 +0200

    Finished

---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,18 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
+    private static final int MAX_NUM = 10;
+
+    @Override
+    public List<Bulldozer> get() {
+        List<Bulldozer> list = new ArrayList<>();
+        for (int i = 0; i < MAX_NUM; i++) {
+            list.add(new Bulldozer());
+        }
+        return list;
+    }
+}
