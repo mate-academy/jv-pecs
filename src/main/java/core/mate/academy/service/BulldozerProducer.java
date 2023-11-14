ff09dd60207bbd75e00d625b140d1527bd62a290
Ivan <lavrinenkoivan51@gmail.com>
2023-11-15 00:09:26 +0100

    MachineProducer implementations created, MachineServiceImpl improved

---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,20 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
+    BulldozerProducer() {
+
+    }
+
+    @Override
+    public List<Bulldozer> get() {
+        List<Bulldozer> bulldozers = new ArrayList<>();
+        bulldozers.add(new Bulldozer());
+        bulldozers.add(new Bulldozer());
+        bulldozers.add(new Bulldozer());
+        return bulldozers;
+    }
+}
---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,20 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    ExcavatorProducer() {
+
+    }
+
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        excavators.add(new Excavator());
+        excavators.add(new Excavator());
+        excavators.add(new Excavator());
+        return excavators;
+    }
+}
