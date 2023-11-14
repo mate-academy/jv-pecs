69def68728354bb6700e316085b3fe6a00ce13c5
AdamMudrak <mudrets2000@gmail.com>
2023-11-15 00:02:48 +0200

    Parametrize MachineProducer

--- src/main/java/core/mate/academy/service/producers/MachineProducer.java
+++ src/main/java/core/mate/academy/service/producers/MachineProducer.java
@@ -4,9 +4,10 @@ import core.mate.academy.model.Machine;
 
 import java.util.List;
 
-public interface MachineProducer {
+public interface MachineProducer<Machine> {
     /**
      * In your implementations - create 2-3 models, add them to the list and return
+     *
      * @return - the list of models
      */
     List<? extends Machine> get();
