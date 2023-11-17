bc4d76fd6e57028e7ecb2faad930b76e9963b16b
AdamMudrak <mudrets2000@gmail.com>
2023-11-17 23:16:53 +0200

    Improve codestyle

--- src/main/java/core/mate/academy/service/producers/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/producers/BulldozerProducer.java
@@ -5,10 +5,10 @@ import core.mate.academy.model.Bulldozer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer<Bulldozer>{
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
-        List<Bulldozer> bulldozers= new ArrayList<>();
+        List<Bulldozer> bulldozers = new ArrayList<>();
         bulldozers.add(new Bulldozer("V6 diesel", 40, false));
         bulldozers.add(new Bulldozer("Electrical", 35, true));
         return bulldozers;
--- src/main/java/core/mate/academy/service/producers/TruckProducer.java
+++ src/main/java/core/mate/academy/service/producers/TruckProducer.java
@@ -1,16 +1,14 @@
 package core.mate.academy.service.producers;
 
-import core.mate.academy.model.Excavator;
-import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TruckProducer implements MachineProducer<Truck>{
+public class TruckProducer implements MachineProducer<Truck> {
     @Override
     public List<Truck> get() {
-        List<Truck> trucks= new ArrayList<>();
+        List<Truck> trucks = new ArrayList<>();
         trucks.add(new Truck("V6 diesel", 120, false));
         trucks.add(new Truck("V8 diesel", 150, true));
         return trucks;
