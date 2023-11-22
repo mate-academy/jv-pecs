cf783a1aeef46ef0d83f9ee54ae2e8049a39d099
Nataliia <gzodchie@gmail.com>
2023-11-22 17:23:51 +0200

    added jv-pecs solution

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,41 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.Workable;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+
+    @Override
+    public List<T> getAll(Class<? extends T> type) {
+        if (Bulldozer.class.equals(type)) {
+            return (List<T>) new MachineProducerBulldozer().get();
+        } else if (Excavator.class.equals(type)) {
+            return (List<T>) new MachineProducerExcavator().get();
+        } else if (Truck.class.equals(type)) {
+            return (List<T>) new MachineProducerTruck().get();
+        }
+        return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Workable> machines) {
+        for (Workable machine : machines) {
+            machine.doWork();
+        }
+    }
 }
