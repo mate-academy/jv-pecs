1ab29cf737cd9817096f2c9817b2b009e63f693f
Snizhana Sosiedova <snejik1111@gmail.com>
2023-11-28 16:40:00 +0200

    Parameterized classes and methods, added realization of methods needed.

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,11 +5,27 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String model;
+
     public Excavator() {
+        super();
+    }
+
+    public Excavator(String name, String color, String model) {
+        super(name, color);
+        this.model = model;
     }
 
     @Override
     public void doWork() {
         System.out.println("Excavator started to work");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 }
