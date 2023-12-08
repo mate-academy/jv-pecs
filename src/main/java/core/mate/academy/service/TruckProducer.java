c75d436869b5b5a856347e1bcbf13fb0a556aa47
Nonzeq <bgdn.kobylchak@gmail.com>
2023-12-08 18:17:58 +0200

    implement MachineServise

---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,12 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Machine> {
+    @Override
+    public List<Machine> get() {
+        return List.of(new Truck(), new Truck());
+    }
+}
