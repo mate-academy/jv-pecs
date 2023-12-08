38566bf215db8f71657527e3900ce5e4ca26f57f
Nonzeq <bgdn.kobylchak@gmail.com>
2023-12-08 18:37:10 +0200

    implement MachineServise

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,7 +1,6 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-import core.mate.academy.model.Machine;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
