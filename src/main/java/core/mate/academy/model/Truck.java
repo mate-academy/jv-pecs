57eb3911956b3c92830c6f84c447cec3787c0131
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-12-12 12:43:55 +0200

    1-st commit 3 tests passed

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,15 +1,66 @@
 package core.mate.academy.model;
 
+import core.mate.academy.service.MachineProducer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Add some custom fields that could be only in Truck
  * Do not remove no-field constructor
  */
-public class Truck extends Machine {
+public class Truck extends Machine implements MachineProducer {
+    private int liftCapacity;
+    private int speed;
+
+    public int getLiftCapacity() {
+        return liftCapacity;
+    }
+
+    public void setLiftCapacity(int liftCapacity) {
+        this.liftCapacity = liftCapacity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
     public Truck() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Truck started to work");
+    }
+
+    @Override
+    public List<Truck> get() {
+        Truck truck1 = new Truck();
+        truck1.liftCapacity = 20;
+        truck1.speed = 50;
+        truck1.setName("Volvo");
+        truck1.setColor("yellow");
+
+        Truck truck2 = new Truck();
+        truck2.liftCapacity = 30;
+        truck2.speed = 40;
+        truck2.setName("MAN");
+        truck2.setColor("red");
+
+        Truck truck3 = new Truck();
+        truck3.liftCapacity = 35;
+        truck3.speed = 35;
+        truck3.setName("Komatsu");
+        truck3.setColor("grey");
+
+        List<Truck> trucks = new ArrayList<>();
+        trucks.add(truck1);
+        trucks.add(truck2);
+        trucks.add(truck3);
+        return trucks;
     }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,8 +1,26 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService {
 
+    @Override
+    public List getAll(Class type) {
+        return null;
+    }
+
+    @Override
+    public void fill(List machines, Object value) {
+
+    }
+
+    @Override
+    public void startWorking(List list) {
+
+    }
 }
