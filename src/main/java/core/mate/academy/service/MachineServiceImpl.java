8a793328bd262ea973c5220406c47b65b51e8158
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-12-12 18:40:39 +0200

    3-st commit 7 tests passed

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,9 +1,11 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,8 +15,17 @@ public class MachineServiceImpl implements MachineService {
 
     @Override
     public List<? extends Machine> getAll(Class type) {
-        MachineProducer machineProducer = new Bulldozer();
-        return machineProducer.get();
+        String typeMachine = type.getSimpleName();
+        switch (typeMachine) {
+            case "Truck":
+                return new Truck().get();
+            case "Excavator":
+                return new Excavator().get();
+            case "Bulldozer":
+                return new Bulldozer().get();
+            default:
+                return new ArrayList<>();
+        }
     }
 
     @Override
