76c7c7177c1862fb9a017d151bae6d3b890f593f
Anna Suprun <evgenevnaanna989@gmail.com>
2024-01-03 19:56:07 +0200

    created producers, fields in Machine classes, methods in MachineServiceImpl

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,15 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private String truckType;
+    private int loadCapacity;
+
     public Truck() {
+    }
+
+    public Truck(String truckType, int loadCapacity) {
+        this.truckType = truckType;
+        this.loadCapacity = loadCapacity;
     }
 
     @Override
