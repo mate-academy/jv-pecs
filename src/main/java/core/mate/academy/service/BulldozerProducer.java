7ac6d04494dd3623a361b246e5bc1b91a5e21ec2
Orest 40th <student2528@bmet.org.ua>
2024-01-13 19:04:02 +0200

    maven corrections

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -1,11 +1,10 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer<Bulldozer>{
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> getMachines() {
         List<Bulldozer> bulldozers = new ArrayList<>();
