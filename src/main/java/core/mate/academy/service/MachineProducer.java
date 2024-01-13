d0a661bdc17e88b44ee4cf573e0a2cb550f29255
Orest 40th <student2528@bmet.org.ua>
2024-01-13 18:47:35 +0200

    reworked

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,31 +1,10 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
+
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface MachineProducer<T extends Machine> {
-    static List<Bulldozer> getBulldozers() {
-        List<Bulldozer> bulldozers = new ArrayList<>();
-        bulldozers.add(new Bulldozer());
-        bulldozers.add(new Bulldozer());
-        return bulldozers;
-    }
+    List<? extends Machine> getMachines();
 
-    static List<Excavator> getExcavators() {
-        List<Excavator> excavators = new ArrayList<>();
-        excavators.add(new Excavator());
-        excavators.add(new Excavator());
-        return excavators;
-    }
-
-    static List<Truck> getTrucks() {
-        List<Truck> trucks = new ArrayList<>();
-        trucks.add(new Truck());
-        trucks.add(new Truck());
-        return trucks;
-    }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,41 +4,36 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * ARE THE INITIAL getALL TESTS FOR LISTS FAULTY????
- *?
- * ?
- *  ?
- *   ?
- */
-public class MachineServiceImpl implements MachineService<Machine> {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
     @Override
-     public List<? extends Machine> getAll(Class<? extends Machine> type) {
+    public List<Machine> getAll(Class<? extends T> type) {
         if (type == Bulldozer.class) {
-            return MachineProducer.getBulldozers();
+            return new ArrayList<>(new BulldozerProducer().getMachines());
         }
         if (type == Excavator.class) {
-            return MachineProducer.getExcavators();
+            return new ArrayList<>(new ExcavatorProducer().getMachines());
         }
         if (type == Truck.class) {
-            return MachineProducer.getTrucks();
+            return new ArrayList<>(new TruckProducer().getMachines());
         }
         return Collections.emptyList();
     }
 
     @Override
-    public void fill(List<? super Machine> machines, Machine value) {
+    public void fill(List<? super T> machines, T value) {
         for (int i = 0; i < machines.size(); i++) {
             machines.set(i, value);
         }
     }
 
     @Override
-    public void startWorking(List<? extends Machine> machines) {
-        for (Machine machine : machines) {
+    public void startWorking(List<? extends T> machines) {
+        for (T machine : machines) {
             machine.doWork();
         }
     }
