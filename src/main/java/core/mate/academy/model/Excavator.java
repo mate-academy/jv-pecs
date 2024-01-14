1d35d81b2d4650d5bd50d060f0e88950de40a1f2
Olha <olga.olegovna2802@online.ua>
2024-01-14 20:50:06 +0000

    PECS solution

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,21 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private int maxSpeed;
+
     public Excavator() {
+    }
+
+    public Excavator(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
     }
 
     @Override
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,21 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private String typeOfDump;
+
     public Truck() {
+    }
+
+    public Truck(String typeOfDump) {
+        this.typeOfDump = typeOfDump;
+    }
+
+    public String getTypeOfDump() {
+        return typeOfDump;
+    }
+
+    public void setTypeOfDump(String typeOfDump) {
+        this.typeOfDump = typeOfDump;
     }
 
     @Override
