8a11d58644d471bf0d11bb47a33ffe8340f48fce
Olha <olga.olegovna2802@online.ua>
2024-01-15 21:08:01 +0000

    PECS solution, fixed mistakes

--- src/main/java/core/mate/academy/producers/TruckProducer.java
+++ src/main/java/core/mate/academy/producers/TruckProducer.java
@@ -1,5 +1,6 @@
-package core.mate.academy.model;
+package core.mate.academy.producers;
 
+import core.mate.academy.model.Truck;
 import core.mate.academy.service.MachineProducer;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,12 +1,12 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.BulldozerProducer;
 import core.mate.academy.model.Excavator;
-import core.mate.academy.model.ExcavatorProducer;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
-import core.mate.academy.model.TruckProducer;
+import core.mate.academy.producers.BulldozerProducer;
+import core.mate.academy.producers.ExcavatorProducer;
+import core.mate.academy.producers.TruckProducer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,7 +14,6 @@ import java.util.List;
 public class MachineServiceImpl implements MachineService<Machine> {
     @Override
         public List<Machine> getAll(Class<? extends Machine> type) {
-
         if (type == Bulldozer.class) {
             return new ArrayList<>(new BulldozerProducer().get());
         }
