b691c851885f845a3229d60128a29721d93800ef
Anton <StarAntonU@gmail.com>
2024-01-26 21:52:50 +0200

    add values in class MachineServiceImpl, change PECS in interface MachineProducer and MachineService

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,14 +1,12 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-import core.mate.academy.model.Machine;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
-
     @Override
-    public List<? extends Machine> get() {
+    public List<Excavator> get() {
         List<Excavator> excavators = new ArrayList<>();
         excavators.add(new Excavator("u55-4"));
         excavators.add(new Excavator("kx06-1"));
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,6 +1,9 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -11,12 +14,16 @@ import java.util.Map;
  * Your implementation of MachineService.
  */
 public class MachineServiceImpl implements MachineService<Machine> {
+    private MachineProducer<Truck> truckMachineProducer = new TruckProducer();
+    private MachineProducer<Excavator> excavatorMachineProducer = new ExcavatorProducer();
+    private MachineProducer<Bulldozer> bulldozerMachineProducer = new BulldozerProducer();
+
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         Map<String, MachineProducer<? extends Machine>> allMachines = new HashMap<>();
-        allMachines.put("Truck", new TruckProducer());
-        allMachines.put("Excavator", new ExcavatorProducer());
-        allMachines.put("Bulldozer", new BulldozerProducer());
+        allMachines.put("Truck", truckMachineProducer);
+        allMachines.put("Excavator", excavatorMachineProducer);
+        allMachines.put("Bulldozer", bulldozerMachineProducer);
         List<? extends Machine> machines = null;
         for (String key : allMachines.keySet()) {
             if (key.equals(type.getSimpleName())) {
