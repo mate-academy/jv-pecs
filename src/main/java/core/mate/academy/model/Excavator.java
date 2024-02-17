12745c013616dee84447afc4c9fd12412a5ebdc9
Milosz <szwajkowski.milosz@gmail.com>
2024-02-17 23:37:11 +0100

    Solved jv-pecs task
    Created all required machine classes and implemented interfaces

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,31 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String armRange;
+    private String shovelCapacity;
+
     public Excavator() {
+    }
+
+    public Excavator(String armRange, String shovelCapacity) {
+        this.armRange = armRange;
+        this.shovelCapacity = shovelCapacity;
+    }
+
+    public String getArmRange() {
+        return armRange;
+    }
+
+    public void setArmRange(String armRange) {
+        this.armRange = armRange;
+    }
+
+    public String getShovelCapacity() {
+        return shovelCapacity;
+    }
+
+    public void setShovelCapacity(String shovelCapacity) {
+        this.shovelCapacity = shovelCapacity;
     }
 
     @Override
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,40 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        MachineProducer<? extends Machine> producer = null;
+        if (type == Bulldozer.class) {
+            producer = new BulldozerProducer();
+        }
+        if (type == Excavator.class) {
+            producer = new ExcavatorProducer();
+        }
+        if (type == Truck.class) {
+            producer = new TruckProducer();
+        }
+        return producer != null ? new ArrayList<>(producer.get()) : List.of();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Object value) {
+        machines.replaceAll(ignored -> (Machine) value);
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
