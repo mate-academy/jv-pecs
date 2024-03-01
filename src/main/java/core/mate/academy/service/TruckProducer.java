4fd867acccaf11194c724c550e38f38409e5726d
Valentyn Sharshon <woosay333@gmail.com>
2024-03-01 14:19:09 +0200

    created TruckProducer class with implementation of method get()

---
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -0,0 +1,17 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Truck;
+import core.mate.academy.producer.MachineProducer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TruckProducer implements MachineProducer<Truck> {
+    @Override
+    public List<Truck> get() {
+        List<Truck> truckList = new ArrayList<>();
+        truckList.add(new Truck(18.2, 4.5, 7));
+        truckList.add(new Truck(16.6, 4.2, 4));
+        truckList.add(new Truck(18, 5.5, 10));
+        return truckList;
+    }
+}
