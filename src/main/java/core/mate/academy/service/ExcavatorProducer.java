5a78893eb7bb49fba14328a32da3e5797232c619
Vasyl Arabchuk <arabon3@gmail.com>
2024-03-08 12:29:41 +0200

    Remove empty lines

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -4,10 +4,8 @@ import core.mate.academy.model.Excavator;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
-
     @Override
     public List<Excavator> get() {
-
         return List.of(new Excavator(),
                 new Excavator(), new Excavator());
     }
